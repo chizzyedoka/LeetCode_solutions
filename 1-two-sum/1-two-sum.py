d81043508112e156934be030bdaa388e13d34aa4
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2022-09-27 05:50:59 +0100

    Time: 6054 ms (14.66%), Space: 15 MB (77.14%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        
+        
+     
         output = []
         for i in range(len(nums)): # loop through nums
             expected = target - nums[i]
             for j in range(i+1,len(nums)):
                 if expected == nums[j]:
-                    output.append(i)
-                    output.append(j)
-            """if expected in nums[i+1:]:
-                output.append(i)"""
+                    return [i,j]
         return output
+        
         
