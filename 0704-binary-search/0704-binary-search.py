6b6e9a2036139ef68b914080939d46627927c8f8
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-07-28 19:21:05 +0100

    Time: 208 ms (99.92%), Space: 17.8 MB (93.51%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,14 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l,r = 0, len(nums)-1
-        while l <= r:
-            middle = (l+r)//2
-            if nums[middle] == target:
-                return middle
-            elif nums[middle] > target: # target is in the first half
-                r = middle -1
-            else:
-                l = middle +1
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target: #number to find is in first half
+                r = mid-1
+            elif nums[mid] < target:
+                l = mid+1
         return -1
-                
-        
