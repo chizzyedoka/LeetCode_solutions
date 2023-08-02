1142c493d2ce4916c95dc47697410f2d769f0dd9
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-08-02 22:49:35 +0100

    Time: 79 ms (38.11%), Space: 45 MB (21.66%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    nums.sort((a,b) => {return a-b})
+    for (let i = 0; i < nums.length+1;i++){
+        if(nums[i] != i) {return i}
+    }
+    
+};
