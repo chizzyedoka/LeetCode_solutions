784d8d5b7c58e1a15a39d9e7aaf6ce1330379a48
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-08-04 16:46:57 +0100

    Time: 14 ms (90.14%), Space: 13.2 MB (62.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,7 +1,13 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            min_index = i
+            for j in range(i+1,len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            nums[i], nums[min_index] = nums[min_index], nums[i]
         
-        """
-        nums.sort()
