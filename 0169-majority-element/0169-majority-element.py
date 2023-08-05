8348c71206ed753e5a12af0d6f9bbf50ed909241
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-08-05 21:14:18 +0100

    Time: 143 ms (98.25%), Space: 17.8 MB (96.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,25 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        hashMap = {}
-        my_max = float('-inf')
-        if len(nums) == 1:
-            return nums[0]
-        for i in range(len(nums)):
-            if nums[i] not in hashMap:
-                hashMap[nums[i]] = 0
-            hashMap[nums[i]] += 1
-            if hashMap[nums[i]] > my_max:
-                    my_max,result = hashMap[nums[i]],nums[i]
-        print(hashMap)
-        return result
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         hashMap = {}
+#         my_max = float('-inf')
+#         if len(nums) == 1:
+#             return nums[0]
+#         for i in range(len(nums)):
+#             if nums[i] not in hashMap:
+#                 hashMap[nums[i]] = 0
+#             hashMap[nums[i]] += 1
+#             if hashMap[nums[i]] > my_max:
+#                     my_max,result = hashMap[nums[i]],nums[i]
+#         return result
             
-        
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
