8e03e8e71269446187726877e5583b1409e9424e
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-08-08 16:37:38 +0100

    Time: 48 ms (71.15%), Space: 16.2 MB (82.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -16,16 +16,16 @@ class Solution:
                 list2 = list2.next
             head = head.next
 
-        if list1 and not list2:
-            while list1:
-                head.next = list1
-                list1 = list1.next
-                head = head.next
-        if list2 and not list1:
-            while list2:
-                head.next = list2
-                list2 = list2.next
-                head = head.next
+        #if list1 and not list2:
+        while list1:
+            head.next = list1
+            list1 = list1.next
+            head = head.next
+        #if list2 and not list1:
+        while list2:
+            head.next = list2
+            list2 = list2.next
+            head = head.next
         return list3.next
             
                 
