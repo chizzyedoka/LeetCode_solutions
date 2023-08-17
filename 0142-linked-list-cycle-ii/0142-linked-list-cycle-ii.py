3443c58b42ee2e315a2f4e0547044cbe792b3612
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-08-17 14:29:18 +0100

    Time: 45 ms (97.12%), Space: 20.7 MB (34.05%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -4,56 +4,32 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         temp = head
+#         hashMap = {}
+#         while temp:
+#             if temp in hashMap:
+#                 return temp
+#             hashMap[temp] = True
+#             temp = temp.next
+#         return None
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
-        hashMap = {}
-        while temp:
-            if temp in hashMap:
-                return temp
-            hashMap[temp] = True
-            temp = temp.next
-        return None
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # slow = head
-        # fast = head
-        # cycle_length = 0
-        # while fast and fast.next:
-        #     slow = slow.next
-        #     fast = fast.next.next
-        #     if slow == fast:
-        #         current = slow
-        #         while current.next != slow:
-        #             current = current.next
-        #             cycle_length += 1
-        #     ptrOne = head
-        #     ptrTwo = head
-        #     while cycle_length > 0:
-        #         ptrTwo = ptrTwo.next
-        #         cycle_length -= 1
-        #     while ptrOne != ptrTwo:
-        #         ptrOne = ptrOne.next
-        #         ptrTwo = ptrTwo.next
-        #     return ptrOne
-        # return None
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        if not fast or not fast.next:
+            return None
+        ptrOne = head
+        while ptrOne != slow:
+                ptrOne = ptrOne.next
+                slow = slow.next
+        return ptrOne
+    
     
     
