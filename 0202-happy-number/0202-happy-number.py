e200112d7429caec4551cd1f0fac46eb609e4aa6
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-08-18 08:42:33 +0100

    Time: 46 ms (63.46%), Space: 16.2 MB (68.73%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -7,13 +7,26 @@ class Solution:
                 n //= 10
                 digit_sum += digit**2
             return digit_sum
-        seen = set()
-        while n != 1:
-            seen.add(n)
-            n = digit_sum(n)
-            if n in seen:
-                return False
-        return True
+        slow = n
+        fast = digit_sum(n)
+        while fast != 1 and slow != fast:
+            slow = digit_sum(slow)
+            fast = digit_sum(digit_sum(fast))
+        return fast == 1
+        # def digit_sum(n):
+        #     digit_sum = 0
+        #     while n > 0:
+        #         digit = n % 10
+        #         n //= 10
+        #         digit_sum += digit**2
+        #     return digit_sum
+        # seen = set()
+        # while n != 1:
+        #     seen.add(n)
+        #     n = digit_sum(n)
+        #     if n in seen:
+        #         return False
+        # return True
         
     
         
