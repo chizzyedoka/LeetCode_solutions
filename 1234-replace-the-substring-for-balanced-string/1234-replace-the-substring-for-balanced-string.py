b6ae5ae355df89430739c1a571915364687ad5dd
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-09-08 23:01:36 +0100

    Time: 145 ms (95.99%), Space: 17.1 MB (23.41%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,32 @@
+# class Solution:
+#     def balancedString(self, s: str) -> int:
+#         hashMap = {}
+#         for i in range(len(s)):
+#             if not hashMap[s[i]]:
+#                 hashMap[s[i]] = 0
+#             hashMap[s[i]] += 1
+class Solution:
+    def balancedString(self, s: str) -> int:
+        counter = collections.Counter(s)
+        n = len(s) // 4
+        extras = {}
+        for key in counter:
+            if counter[key] > n:
+                extras[key] = counter[key] - n
+        
+        if not extras: return 0
+        i = 0
+        res = len(s)
+        for j in range(len(s)):
+            if s[j] in extras:
+                extras[s[j]] -= 1
+            
+            while max(extras.values()) <= 0:
+                res = min(res, j-i+1)
+                if s[i] in extras:
+                    extras[s[i]] += 1
+                i += 1
+                
+                
+        return res
+        
