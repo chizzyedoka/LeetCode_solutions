b8ed22302856c048f202d5832a47f8ffffdd81dd
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-09-23 10:29:19 +0100

    Time: 54 ms (5.30%), Space: 16.2 MB (89.85%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -4,47 +4,43 @@
 #         self.val = val
 #         self.next = next
 
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # time complexity - O(n)
-        # space complexity - O(1)
-        p1 = head
-        p2 = head
-        for i in range(n):
-            p2 = p2.next
-        prev = p1
-        if not p2:
+        # get the length of the linked list
+        temp = head
+        size = 0
+        while temp:
+            size += 1
+            temp = temp.next
+        print("size is:",size)
+        
+        # subtract n from length to get the node to remove
+        index_to_remove = size - n - 1
+        print("index of node to remove is:",index_to_remove)
+        
+        # edge case
+        if size == n:
             return head.next
-        while p2:
-            prev = p1
-            p1 = p1.next
-            p2 = p2.next
-        if prev and p1:
-            prev.next = p1.next
+        
+        # remove the node
+        curr = head
+        for i in range(index_to_remove):
+            curr = curr.next
+            print(curr)
+        curr.next = curr.next.next
         return head
-
-# class Solution:
-#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-#         # get the length of the linked list
-#         temp = head
-#         size = 0
-#         while temp:
-#             size += 1
-#             temp = temp.next
-#         print("size is:",size)
-#         # subtract n from length to get the node to remove
-#         node_to_remove = size - n
-#         print("node to nremove is:",node_to_remove)
-#         # remove the node
-#         count = 0
-#         dummy_head = ListNode(0)
-#         dummy_head = head
-#         print(dummy_head)
-#         while dummy_head:
-#             if count != node_to_remove:
-#                 dummy_head = dummy_head.next
-#             else:
-#                 dummy_head = dummy_head.next.next
-#             count += 1
-#         print(head)
-#         return dummy_head
+        
+        
+        # count = 0
+        # dummy = ListNode()
+        # ptr = dummy
+        # while head:
+        #     if count != index_to_remove-1:
+        #         ptr.next = head
+        #         ptr = ptr.next
+        #         head = head.next
+        #     else:
+        #         head = head.next
+        #     count += 1
+        # return dummy.next
