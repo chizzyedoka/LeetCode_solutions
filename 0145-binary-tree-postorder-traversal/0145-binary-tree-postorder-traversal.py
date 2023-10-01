42c0f2382d3c53b16cd6d0f50d4f86e1a065e2cf
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-10-01 14:08:45 +0100

    Time: 37 ms (66.87%), Space: 16.2 MB (63.18%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -6,9 +6,20 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root: #root is not null
-            return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
-        return []
+        result = []
+        def postorder(root):
+            if not root:
+                return
+            postorder(root.left)
+            postorder(root.right)
+            result.append(root.val)
+        postorder(root)
+        return result
+        
+        # recursive solution
+        # if root: #root is not null
+        #     return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+        # return []
     
             
             
