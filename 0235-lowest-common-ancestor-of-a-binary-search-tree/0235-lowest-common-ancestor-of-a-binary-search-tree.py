77d6e3780547cb7aa73b3039ade379e05ccb785f
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-11-08 15:23:30 +0100

    Time: 76 ms (36.83%), Space: 20.7 MB (62.84%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,44 +1,19 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, x):
-# #         self.val = x
-# #         self.left = None
-# #         self.right = None
-
-# class Solution:
-#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-#         p_val = p.val
-#         q_val = q.val
-#         if (p_val < root.val) and (q_val < root.val):
-#             self.lowestCommonAncestor(root.left,p,q)
-#         elif (p_val > root.val) and (q_val > root.val):
-#             self.lowestCommonAncestor(root.right,p,q)
-#         else:
-#             return root
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        # Value of current node or parent node.
-        parent_val = root.val
-
-        # Value of p
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         p_val = p.val
-
-        # Value of q
         q_val = q.val
-
-        # If both p and q are greater than parent
-        if p_val > parent_val and q_val > parent_val:    
-            return self.lowestCommonAncestor(root.right, p, q)
-        # If both p and q are lesser than parent
-        elif p_val < parent_val and q_val < parent_val:    
-            return self.lowestCommonAncestor(root.left, p, q)
-        # We have found the split point, i.e. the LCA node.
+        if (p_val > root.val) and (q_val > root.val):
+            return self.lowestCommonAncestor(root.right,p,q)
+        elif (p_val < root.val) and (q_val < root.val):
+            return self.lowestCommonAncestor(root.left,p,q)
         else:
             return root
+
+
