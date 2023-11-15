002d09fe6aea5eaa9189853faae745ab66c31a56
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-11-15 20:38:42 +0100

    Time: 40 ms (43.96%), Space: 16.3 MB (8.33%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,18 +6,62 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        node = root
         result = []
-        nodeStack = []
-        current = root
-        while nodeStack or current:
-            if current:
-                nodeStack.append(current)
-                current = current.left
-            else:
-                current = nodeStack.pop()
-                result.append(current.val)
-                current = current.right
+        stack = []
+        while stack or node:
+            while node:
+                stack.append(node)
+                node= node.left
+            node = stack.pop()
+            result.append(node.val)
+            node = node.right
         return result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # result = []
+        # nodeStack = []
+        # current = root
+        # while nodeStack or current:
+        #     if current:
+        #         nodeStack.append(current)
+        #         current = current.left
+        #     else:
+        #         current = nodeStack.pop()
+        #         result.append(current.val)
+        #         current = current.right
+        # return result
+    
+        # stack = []
+        # result = []
+        # curr = root
+        # while stack or curr:
+        #     while curr:
+        #         stack.append(curr)
+        #         curr = curr.left
+        #     curr = stack.pop()
+        #     result.append(curr.val)
+        #     curr = curr.right
+        # return result
+        
 #         if root:
 #             return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
 #         return []
