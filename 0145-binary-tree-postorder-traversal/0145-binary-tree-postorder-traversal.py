65101038bab868500abe012740bbda83f092296a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2023-11-19 23:50:59 +0100

    Time: 26 ms (99.10%), Space: 16.4 MB (10.54%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -1,26 +1,44 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-        def postorder(root):
-            if not root:
-                return
-            postorder(root.left)
-            postorder(root.right)
-            result.append(root.val)
-        postorder(root)
-        return result
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+
         
-        # recursive solution
-        # if root: #root is not null
-        #     return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
-        # return []
-    
+#         # recursive solution
+#         # result = []
+#         # def postorder(root):
+#         #     if not root:
+#         #         return
+#         #     postorder(root.left)
+#         #     postorder(root.right)
+#         #     result.append(root.val)
+#         # postorder(root)
+#         # return result
+        
+#         # recursive solution
+#         # if root: #root is not null
+#         #     return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+#         # return []
+
+
+class Solution(object):
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+
+        stack, output = [root, ], []
+        while stack:
+            root = stack.pop()
+            output.append(root.val)
+            if root.left is not None:
+                stack.append(root.left)
+            if root.right is not None:
+                stack.append(root.right)
+                
+        return output[::-1]
+
             
             
         
