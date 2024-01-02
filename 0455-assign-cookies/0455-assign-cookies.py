6d44867065bb1983ac6fb2f2aa43817404557714
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-01-02 06:42:22 +0100

    Time: 237 ms (11.63%), Space: 19.3 MB (10.05%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,21 +1,65 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-
         g.sort()
         s.sort()
-
-        a = len(g) - 1
-        b = len(s) - 1
-        maxs = 0
-
-        while a >= 0 and b >= 0:
-            if s[b] >= g[a]:
-                maxs += 1
-                a -= 1
-                b -= 1
+        n, m = len(g)-1, len(s)-1
+        count = 0
+        while n >= 0 and m >= 0:
+            if s[m] >= g[n]:
+                count += 1
+                n -= 1
+                m -= 1
             else:
-                a -= 1
-        return maxs
+                n -=1
+        return count
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         g.sort()
+#         s.sort()
+
+#         a = len(g) - 1
+#         b = len(s) - 1
+#         maxs = 0
+
+#         while a >= 0 and b >= 0:
+#             if s[b] >= g[a]:
+#                 maxs += 1
+#                 a -= 1
+#                 b -= 1
+#             else:
+#                 a -= 1
+#         return maxs
 # class Solution:
 #     def findContentChildren(self, g: List[int], s: List[int]) -> int:
 #         count = 0
