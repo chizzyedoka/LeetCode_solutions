b1b2d37ef452a43d15acedef5bf55369953ef232
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-01-16 06:36:06 +0100

    Time: 83 ms (87.75%), Space: 28.7 MB (60.28%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,17 +6,42 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        # create set A and set B to store all the values seen in both LinkedList
-        # trans
-        l1 = headA
-        l2 = headB
-        while l1 != l2:
-            if l1:
-                l1 = l1.next
-            else:
-                l1 = headB
-            if l2:
-                l2 = l2.next
-            else:
-                l2 = headA
-        return l1
+        # using hashMap
+        seen = set()
+        aptr = headA
+        bptr = headB
+        while aptr:
+            seen.add(aptr)
+            aptr = aptr.next
+        while bptr:
+            if bptr in seen:
+                return bptr
+            bptr = bptr.next
+        return None
+        # l1 = headA
+        # l2 = headB
+        # while l1 != l2:
+        #     if l1:
+        #         l1 = l1.next
+        #     else:
+        #         l1 = headB
+        #     if l2:
+        #         l2 = l2.next
+        #     else:
+        #         l2 = headA
+        # return l1
+    
+#         if headA is None or headB is None:
+#             return None
+        
+#         intersection = set()
+#         while headA is not None:
+#             intersection.add(headA)
+#             headA = headA.next
+        
+#         while headB is not None:
+#             if headB in intersection:
+#                 return headB
+#             headB = headB.next
+        
+#         return Non
