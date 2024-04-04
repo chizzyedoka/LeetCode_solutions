1dfe381b519f5b7f642c75b989c985cf85f4bfd6
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-04 09:08:38 +0100

    Time: 295 ms (45.66%), Space: 32.1 MB (70.89%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,12 +6,89 @@
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        vals = []
-        current_node = head
-        while current_node is not None:
-            vals.append(current_node.val)
-            current_node = current_node.next
-        return vals == vals[::-1]
+        # Helper function to reverse a linked list
+        def reverse(slow):
+            prev = None
+            while slow:
+                next_node = slow.next
+                slow.next = prev
+                prev = slow
+                slow = next_node
+            return prev
+        
+        if not head or not head.next:
+            return True
+        
+        if not head.next.next:
+            return head.val == head.next.val
+        
+        slow, fast = head, head
+        # Move slow to the middle of the linked list
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Reverse the second half of the linked list
+        reversed_half = reverse(slow.next)
+        
+        # Compare the first half and reversed half of the linked list
+        cur = head
+        while cur and reversed_half:
+            if cur.val != reversed_half.val:
+                return False
+            cur = cur.next
+            reversed_half = reversed_half.next
+        return True
+
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         # helper function
+#         def reverse(slow):
+#             prev = None
+#             while slow:
+#                 next_node = slow.next
+#                 slow.next = prev
+#                 prev = slow
+#                 slow = next_node
+#             return prev
+        
+#         if not head or not head.next:
+#             return True
+        
+#         if not head.next.next:
+#             return head.val == head.next.val
+        
+#         slow,fast = head, head
+#         # get slow to the middle of the LL
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+       
+#         # reverse one half
+#         reversed_half = reverse(slow.next)
+#         slow.next = None  
+        
+#         # compare first_half and reversed_half
+#         cur = head
+#         while (reversed_half and reversed_half.next):
+#             if cur.val != reversed_half.val:
+#                 return False
+#             cur = cur.next
+#             reversed_half = reversed_half.next
+#         return True
+        
+        
+        
+        
+        
+        
+    # def isPalindrome(self, head: ListNode) -> bool:
+    #     vals = []
+    #     current_node = head
+    #     while current_node is not None:
+    #         vals.append(current_node.val)
+    #         current_node = current_node.next
+    #     return vals == vals[::-1]
 #     def isPalindrome(self, head: Optional[ListNode]) -> bool:
 #         slow = head
 #         fast = head
