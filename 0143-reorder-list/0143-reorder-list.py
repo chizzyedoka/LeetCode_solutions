7b0b5a0dec1ab8be3ce2d4c3dae5dc0c60dbe7e3
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-04 08:59:21 +0100

    Time: 59 ms (37.00%), Space: 24.6 MB (50.30%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,75 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def reorderList(self, head: Optional[ListNode]) -> None:
+#         def reverse(head):
+#             prev = None
+#             while head:
+#                 next_node = head.next
+#                 head.next = prev
+#                 prev = head
+#                 head = next_node
+#             return prev
+        
+#         if not head or not head.next:
+#             return # head
+        
+#         # get to the middle of the LL
+#         slow, fast = head, head.next
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+            
+#         # reverse from middle->end of LL
+#         head_second_half = reverse(slow)
+        
+#         # rearrange to produce LL in the required order
+#         head_first_half = head
+#         while head_second_half:
+#             temp1 = head_first_half.next
+#             temp2 = head_second_half.next
+#             head_first_half.next = head_second_half
+#             head_second_half.next = temp1
+#             head_first_half = temp1
+#             head_second_half = temp2
+            
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        def reverse(head):
+            prev = None
+            while head:
+                next_node = head.next
+                head.next = prev
+                prev = head
+                head = next_node
+            return prev
+
+        if not head or not head.next:
+            return
+
+        # get to the middle of the LL
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse from middle->end of LL
+        head_second_half = reverse(slow.next)
+        slow.next = None  # break the link between first and second half
+
+        # rearrange to produce LL in the required order
+        head_first_half = head
+        while head_second_half:
+            temp1 = head_first_half.next
+            temp2 = head_second_half.next
+            head_first_half.next = head_second_half
+            head_second_half.next = temp1
+            head_first_half = temp1
+            head_second_half = temp2
+
+         
+        
+        
