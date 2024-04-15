89c0f36252954c19388d214545281a0e066c875a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-15 17:35:35 +0100

    Time: 49 ms (71.45%), Space: 16.7 MB (35.19%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -6,9 +6,7 @@ class Solution:
         for i in range(len(arr)+k):
             if i+1 not in arr_set:
                 missing.append(i+1)
-                n += 1
-            if n == k:
-                break
+                
             
         return missing[k-1]
             
