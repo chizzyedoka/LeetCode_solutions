6df015fb8602052af53a6b39e3eb1b23a7dd2bef
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-21 03:00:27 +0100

    Time: 42 ms (74.46%), Space: 18.5 MB (24.58%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -6,21 +6,86 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        result = []
-        if not root:
-            return result
         q = deque()
         q.append(root)
-        average = 0
-        while len(q)>0:
-            currLevel = []
-            levelSize = len(q)
-            for _ in range(levelSize):
-                currNode = q.popleft()
-                if currNode.left:
-                    q.append(currNode.left)
-                if currNode.right:
-                    q.append(currNode.right)
-                currLevel.append(currNode.val)
-            result.append(sum(currLevel)/levelSize)
-        return result
+        res = []
+        while q:
+            level_size = len(q)
+            #level = []
+            _sum = 0
+            for _ in range(level_size):
+                node = q.popleft()
+                #level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                _sum += node.val
+            res.append(_sum/level_size)
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        # result = []
+        # if not root:
+        #     return result
+        # q = deque()
+        # q.append(root)
+        # average = 0
+        # while len(q)>0:
+        #     currLevel = []
+        #     levelSize = len(q)
+        #     for _ in range(levelSize):
+        #         currNode = q.popleft()
+        #         if currNode.left:
+        #             q.append(currNode.left)
+        #         if currNode.right:
+        #             q.append(currNode.right)
+        #         currLevel.append(currNode.val)
+        #     result.append(sum(currLevel)/levelSize)
+        # return result
