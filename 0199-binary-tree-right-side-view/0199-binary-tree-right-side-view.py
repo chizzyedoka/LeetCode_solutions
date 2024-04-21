c389d9771c8a0ba8c9cd058fa84791f314ddf02e
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-21 05:16:11 +0100

    Time: 38 ms (46.64%), Space: 16.6 MB (45.17%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,13 +6,56 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root: return []
-        queue = [root]
-        result = []
-        while queue:
-            for _ in range(len(queue)):
-                node = queue.pop(0)
-                if node.left: queue.append(node.left)
-                if node.right: queue.append(node.right)
-            result.append(node.val)
-        return result
+        if not root:
+            return
+        res = []
+        q=deque()
+        q.append(root)
+        while q:
+            level_size = len(q)
+            for _ in range(level_size):
+                node = q.popleft()
+                if _==level_size-1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if not root: return []
+        # queue = [root]
+        # result = []
+        # while queue:
+        #     for _ in range(len(queue)):
+        #         node = queue.pop(0)
+        #         if node.left: queue.append(node.left)
+        #         if node.right: queue.append(node.right)
+        #     result.append(node.val)
+        # return result
