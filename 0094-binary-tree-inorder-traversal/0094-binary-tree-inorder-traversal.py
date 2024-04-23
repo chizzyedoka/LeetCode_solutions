3d7b4ec1bb2945c42cb69970e2d06e1dd3995dcc
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-23 15:34:39 +0100

    Time: 37 ms (44.17%), Space: 16.5 MB (79.71%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -14,16 +14,16 @@ class Solution:
         # return inorder(root)
       
     # recursive ii -> faster, passing the result
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        def inorder(root,res):
-            if not root:
-                return 
-            inorder(root.left,res)
-            res.append(root.val)
-            inorder(root.right,res)
-        inorder(root,res)
-        return res
+    # def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     res = []
+    #     def inorder(root,res):
+    #         if not root:
+    #             return 
+    #         inorder(root.left,res)
+    #         res.append(root.val)
+    #         inorder(root.right,res)
+    #     inorder(root,res)
+    #     return res
     
     # iterative solution
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
@@ -38,75 +38,8 @@ class Solution:
             res.append(cur.val)
             cur=cur.right
         return res
-            
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res = []
-#         stack = []
-#         cur = root
-#         while stack or cur:
-#             while cur:
-#                 stack.append(cur)
-#                 cur = cur.left
-#             cur = stack.pop()
-#             res.append(cur.val)
-#             cur = cur.right
-#         return res
-        
-        
-#         result = []
-#         stack = []
-#         cur = root
-#         while stack or cur:
-#             while cur:
-#                 stack.append(cur)
-#                 cur = cur.left
-#             cur = stack.pop()
-#             result.append(cur.val)
-#             cur = cur.right
-#         return result
-        
-        
-        
-        
-        
-        # result = []
-        # nodeStack = []
-        # current = root
-        # while nodeStack or current:
-        #     if current:
-        #         nodeStack.append(current)
-        #         current = current.left
-        #     else:
-        #         current = nodeStack.pop()
-        #         result.append(current.val)
-        #         current = current.right
-        # return result
     
-        # stack = []
-        # result = []
-        # curr = root
-        # while stack or curr:
-        #     while curr:
-        #         stack.append(curr)
-        #         curr = curr.left
-        #     curr = stack.pop()
-        #     result.append(curr.val)
-        #     curr = curr.right
-        # return result
         
 #         if root:
 #             return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
