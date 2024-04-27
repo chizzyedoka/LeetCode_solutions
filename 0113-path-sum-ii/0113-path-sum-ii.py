1f1e74d31fad55597bd022f4bd049fad4d56d296
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-28 00:17:55 +0100

    Time: 38 ms (83.17%), Space: 17.3 MB (96.90%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -6,21 +6,90 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        self.output = []
+        all_paths = []
+        def dfs(node,cur_sum,cur_path,all_paths):
+            if not node:
+                return 
+            cur_path.append(node.val)
+            if not node.left and not node.right and node.val==cur_sum:
+                all_paths.append(cur_path[:])
+            else:
+                dfs(node.left, cur_sum-node.val, cur_path, all_paths)
+                dfs(node.right, cur_sum-node.val, cur_path, all_paths)
+            cur_path.pop()
+        dfs(root,targetSum,[], all_paths)
+        return all_paths
+    
+    # iterative solution
+#      def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+#             if not root:
+#                 return []
+#             stack = [root]
+#             sum_stack = [root.val]
+#             res = []
+#             path = []
+#             while stack:
+#                 node = stack.pop()
+#                 _sum = sum_stack.pop()
+#                 if not node.left and not node.right and targetSum==_sum:
+#                     # do something
+#                 if node.right:
+#                     stack.append(node.right)
+#                     sum_stack.append(node.right.val++sum)
+#                     path.append(node.val)
+#                 if node.left:
+#                     stack.append(node.left)
+#                     sum_stack.append(node.left.val+_sum)
+#                     path.append(node.val)
+#             return res
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+#         self.output = []
         
-        def dfs(node,path,curSum):
-            if not root:
-                return []
-            curSum += node.val
-            temp = path + [node.val]
-            if node.left:
-                dfs(node.left,temp,curSum)
-            if node.right:
-                dfs(node.right,temp,curSum)
-            if not node.left and not node.right and curSum == targetSum:
-                    self.output.append(temp)
-        dfs(root,[],0)
-        return self.output
+#         def dfs(node,path,curSum):
+#             if not root:
+#                 return []
+#             curSum += node.val
+#             temp = path + [node.val]
+#             if node.left:
+#                 dfs(node.left,temp,curSum)
+#             if node.right:
+#                 dfs(node.right,temp,curSum)
+#             if not node.left and not node.right and curSum == targetSum:
+#                     self.output.append(temp)
+#         dfs(root,[],0)
+#         return self.output
         
         
         # output = []   
