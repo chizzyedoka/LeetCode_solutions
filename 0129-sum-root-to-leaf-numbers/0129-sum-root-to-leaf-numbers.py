d0ab52e4b951ca0d2046341226ccff590358a609
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-28 00:50:15 +0100

    Time: 34 ms (69.10%), Space: 16.5 MB (77.79%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -5,34 +5,47 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        stack = [root]
-        sum_stack = [0]
-        total = 0
-        while stack:
-            node = stack.pop()
-            cur_sum= sum_stack.pop()
-            cur_sum = cur_sum*10 + node.val
-            if node.right:
-                stack.append(node.right)
-                sum_stack.append(cur_sum)
-            if node.left:
-                stack.append(node.left)
-                sum_stack.append(cur_sum)
-            if not node.left and not node.right:
-                total += cur_sum
-        return total
+    #  # recursive solution
+    # def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    #     def dfs(node, path_sum):
+    #         if not node:
+    #             return 0
+    #         path_sum = 10*path_sum + node.val 
+    #         if not node.left and not node.right:
+    #             return path_sum
+    #         left_sum = dfs(root.left,path_sum)
+    #         right_sum = dfs(root.right,path_sum)
+    #         return left_sum + right_sum
+    #     return dfs(root,0)
+    # iterative solution
+    # def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    #     stack = [root]
+    #     sum_stack = [0]
+    #     total = 0
+    #     while stack:
+    #         node = stack.pop()
+    #         cur_sum= sum_stack.pop()
+    #         cur_sum = cur_sum*10 + node.val
+    #         if node.right:
+    #             stack.append(node.right)
+    #             sum_stack.append(cur_sum)
+    #         if node.left:
+    #             stack.append(node.left)
+    #             sum_stack.append(cur_sum)
+    #         if not node.left and not node.right:
+    #             total += cur_sum
+    #     return total
                 
             
-    # def sumNumbers(self, root: Optional[TreeNode]) -> int:
-    #     def dfs(root, cur_sum):
-    #         if not root:
-    #             return 0
-    #         cur_sum = cur_sum*10 + root.val
-    #         if not root.left and not root.right:
-    #             return cur_sum
-    #         left_sum = dfs(root.left, cur_sum)
-    #         right_sum = dfs(root.right, cur_sum)
-    #         return left_sum + right_sum
-    #     return dfs(root, 0)
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, cur_sum):
+            if not root:
+                return 0
+            cur_sum = cur_sum*10 + root.val
+            if not root.left and not root.right:
+                return cur_sum
+            left_sum = dfs(root.left, cur_sum)
+            right_sum = dfs(root.right, cur_sum)
+            return left_sum + right_sum
+        return dfs(root, 0)
         
