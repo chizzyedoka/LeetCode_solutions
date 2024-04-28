43aabf07f920d791fcbf92ff3d6c585a21ede508
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-04-28 09:38:50 +0100

    Time: 34 ms (69.10%), Space: 16.6 MB (23.16%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,17 +6,17 @@
 #         self.right = right
 class Solution:
      # recursive solution
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def dfs(node, path_sum):
-            if not node:
-                return 0
-            path_sum = path_sum*10 + node.val 
-            if not node.left and not node.right:
-                return path_sum
-            left_sum = dfs(node.left,path_sum)
-            right_sum = dfs(node.right,path_sum)
-            return left_sum + right_sum
-        return dfs(root,0)
+    # def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    #     def dfs(node, path_sum):
+    #         if not node:
+    #             return 0
+    #         path_sum = path_sum*10 + node.val 
+    #         if not node.left and not node.right:
+    #             return path_sum
+    #         left_sum = dfs(node.left,path_sum)
+    #         right_sum = dfs(node.right,path_sum)
+    #         return left_sum + right_sum
+    #     return dfs(root,0)
     #def sumNumbers(self, root: Optional[TreeNode]) -> int:
     #     def dfs(root, cur_sum):
     #         if not root:
@@ -29,23 +29,25 @@ class Solution:
     #         return left_sum + right_sum
     #     return dfs(root, 0)
     # iterative solution
-    # def sumNumbers(self, root: Optional[TreeNode]) -> int:
-    #     stack = [root]
-    #     sum_stack = [0]
-    #     total = 0
-    #     while stack:
-    #         node = stack.pop()
-    #         cur_sum= sum_stack.pop()
-    #         cur_sum = cur_sum*10 + node.val
-    #         if node.right:
-    #             stack.append(node.right)
-    #             sum_stack.append(cur_sum)
-    #         if node.left:
-    #             stack.append(node.left)
-    #             sum_stack.append(cur_sum)
-    #         if not node.left and not node.right:
-    #             total += cur_sum
-    #     return total
+    
+    #iterative solution
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        stack = [root]
+        sum_stack = [0]
+        total = 0
+        while stack:
+            node = stack.pop()
+            cur_sum= sum_stack.pop()
+            cur_sum = cur_sum*10 + node.val
+            if node.right:
+                stack.append(node.right)
+                sum_stack.append(cur_sum)
+            if node.left:
+                stack.append(node.left)
+                sum_stack.append(cur_sum)
+            if not node.left and not node.right:
+                total += cur_sum
+        return total
                 
             
     # 
