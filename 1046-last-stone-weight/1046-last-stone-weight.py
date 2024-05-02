12a5cae1eba3371287b577edff0fece247848837
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-02 19:18:13 +0100

    Time: 27 ms (95.91%), Space: 16.6 MB (41.85%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,9 +1,9 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        # if len(stones) == 1:
-        #     return stones[0]
-        # if len(stones) == 0:
-        #     return 0
+        if len(stones) == 1:
+            return stones[0]
+        if len(stones) == 0:
+            return 0
         import heapq
         max_heap = []
         for stone in stones:
