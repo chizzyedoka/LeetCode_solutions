c739464d68dfee39534fa397e66c2ef3e5f7fe20
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-04 20:23:29 +0100

    Time: 92 ms (40.96%), Space: 21.1 MB (73.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,38 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         import heapq
         hashMap = {}
-        result = []
-        if len(nums) == k:
-            return nums
+        res = []
+        maxHeap = []
+        
         for i in range(len(nums)):
             if nums[i] not in hashMap:
                 hashMap[nums[i]] = 0
             hashMap[nums[i]] += 1
-        # for keys,v in hashMap.items():
-        #     if v >= k:
-        #         result.append(keys)
-        return heapq.nlargest(k, hashMap.keys(), key=hashMap.get) 
+        
+        for (key,v) in hashMap.items():
+            heapq.heappush(maxHeap,(-v,key))
+            
+        print(hashMap,k)
+        
+        while k>0:
+            res.append(heapq.heappop(maxHeap)[1])
+            k-=1   
+        return res
+        
+        
+        
+        # import heapq
+        # hashMap = {}
+        # result = []
+        # if len(nums) == k:
+        #     return nums
+        # for i in range(len(nums)):
+        #     if nums[i] not in hashMap:
+        #         hashMap[nums[i]] = 0
+        #     hashMap[nums[i]] += 1
+        # values = list(hashMap.values())
+        # heapq.heapify(values)
+        # return heapq.nlargest(k, hashMap.keys(), key=hashMap.get) 
         
             
             
