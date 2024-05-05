76df04d2e4603e49a80a7ef1ddf522a9feed85d1
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-05 12:14:43 +0100

    Time: 44 ms (16.01%), Space: 16.6 MB (29.95%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,34 +1,55 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        import heapq
-        letterCounter = {}
+        hashMap = {}
         maxHeap = []
+        res = ""
+        for char in s:
+            if char not in hashMap:
+                hashMap[char] = 0
+            hashMap[char] += 1
+        for key in hashMap.keys():
+            heapq.heappush(maxHeap,(-hashMap[key], key))
+        prev_char, prev_freq = None, 0
+        while maxHeap:
+            freq, char = heapq.heappop(maxHeap)
+            if prev_char and -prev_freq >0:
+                heapq.heappush(maxHeap,(prev_freq,prev_char))
+            res += char
+            prev_char = char
+            prev_freq = freq + 1
+        return res if len(s)==len(res) else ""
         
-        for letter in s:
-            if letter not in letterCounter.keys():
-                letterCounter[letter] = 0
-            letterCounter[letter] += 1
+        
+        
+#         import heapq
+#         letterCounter = {}
+#         maxHeap = []
+        
+#         for letter in s:
+#             if letter not in letterCounter.keys():
+#                 letterCounter[letter] = 0
+#             letterCounter[letter] += 1
             
-        for k,v in letterCounter.items():
-            heapq.heappush(maxHeap,[-v,k])
+#         for k,v in letterCounter.items():
+#             heapq.heappush(maxHeap,[-v,k])
         
-        previous = None
-        result = ""
+#         previous = None
+#         result = ""
         
-        while len(maxHeap) > 0:
-            count, letter = heapq.heappop(maxHeap)
-            result += letter
-            count += 1
+#         while len(maxHeap) > 0:
+#             count, letter = heapq.heappop(maxHeap)
+#             result += letter
+#             count += 1
                 
-            if previous:
-                heapq.heappush(maxHeap,previous)
-                previous = None
+#             if previous:
+#                 heapq.heappush(maxHeap,previous)
+#                 previous = None
                 
-            if count != 0:
-                previous = [count,letter]
+#             if count != 0:
+#                 previous = [count,letter]
         
-            if count != 0 and len(maxHeap) == 0:
-                return ""
+#             if count != 0 and len(maxHeap) == 0:
+#                 return ""
             
-        return result
+#         return result
                 
