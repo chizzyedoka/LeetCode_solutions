5e2cd1d7cf47a6a0e5661b89b617eedc46f5cce0
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-14 10:42:07 +0100

    Time: 52 ms (89.61%), Space: 17.8 MB (41.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,45 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = dict()
+        hashmap = {}
         for i in range(len(nums)):
-            value = target - nums[i]
-            if value in  hashMap.keys():
-                return [hashMap[value],i]
-            hashMap[nums[i]] = i
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i,hashmap[complement]]
            
-#         pointer_one = 0
-#         pointer_two = len(nums) - 1
-#         hashMap = dict()
-#         for index, num in enumerate(nums):
-#             if num not in hashMap:
-#                 hashMap[num] = []
-#             hashMap[num].append(index)
-
-#         nums = sorted(nums)
-
-#         while pointer_one < pointer_two:
-#             sum = nums[pointer_one] + nums[pointer_two]
-#             if sum == target:
-#                 if nums[pointer_one] == nums[pointer_two]:
-#                     return [hashMap[nums[pointer_one]][0], hashMap[nums[pointer_two]][1]]
-#                 return [hashMap[nums[pointer_one]][0], hashMap[nums[pointer_two]][0]]
-#             elif sum < target:
-#                 pointer_one += 1
-#             else:
-#                 pointer_two -= 1
-
-
-
-
-
-
-
-        """ for i in range(len(nums)):
-            for j in range(1,len(nums)):
-                if i == j:
-                    continue
-                elif nums[i] == target - nums[j]:
-                    return [i,j]
-                    """
-        
-        
