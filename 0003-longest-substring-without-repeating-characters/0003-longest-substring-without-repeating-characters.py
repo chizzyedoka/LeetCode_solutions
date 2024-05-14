b66648a6a31d7f995b3a4dc0c596acf3997495cb
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-14 13:16:59 +0100

    Time: 65 ms (28.96%), Space: 16.6 MB (60.36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,40 +1,33 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        ans = 0
-        # mp stores the current index of a character
-        mp = {}
-
-        i = 0
-        # try to extend the range [i, j]
-        for j in range(n):
-            if s[j] in mp:
-                i = max(mp[s[j]], i)
-
-            ans = max(ans, j - i + 1)
-            mp[s[j]] = j + 1
-
-        return ans
-    
-    
-    
-    
 # class Solution:
 #     def lengthOfLongestSubstring(self, s: str) -> int:
-#         count = 0
-#         window = ""
-#         longest = float("-inf")
-        
-#         for end in range(len(s)-1):
-#             window += s[end]
-#             if s[end+1] in window:
-#                 window_len = len(window) 
-#                 longest = max(longest,window_len)
-#                 window = ""
-             
-                    
-       #        if longest == float("-inf"):
-#             return len(s)
-#         return longest
+#         n = len(s)
+#         ans = 0
+#         # mp stores the current index of a character
+#         mp = {}
+
+#         i = 0
+#         # try to extend the range [i, j]
+#         for j in range(n):
+#             if s[j] in mp:
+#                 i = max(mp[s[j]], i)
+
+#             ans = max(ans, j - i + 1)
+#             mp[s[j]] = j + 1
+
+#         return ans
+    
+    
+    
+    
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substring = deque()
+        max_substring = 0
+        for char in s:
+            while char in substring:
+                substring.popleft()
+            substring.append(char)
+            max_substring = max(max_substring, len(substring))
+        return max_substring
                 
                 
