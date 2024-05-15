825e5dcd70b499bfbcd97b84bd049dce2ec21878
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-15 17:29:29 +0100

    Time: 58 ms (47.52%), Space: 16.8 MB (19.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -19,17 +19,29 @@
     
     
     
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         substring = deque()
+#         max_substring = 0
+#         for char in s:
+#             while char in substring:
+#                 substring.popleft()
+#             substring.append(char)
+#             max_substring = max(max_substring, len(substring))
+#         return max_substring
+
+    
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        substring = deque()
-        max_substring = 0
-        for char in s:
-            while char in substring:
-                substring.popleft()
-            substring.append(char)
-            max_substring = max(max_substring, len(substring))
-        return max_substring
-    
+        window_start, max_length = 0,0
+        char_index = {}
+        for window_end in range(len(s)):
+            right_char = s[window_end]
+            if right_char in char_index:
+                window_start = max(window_start, char_index[right_char]+1)
+            char_index[right_char] = window_end
+            max_length = max(max_length, window_end-window_start+1)
+        return max_length
 
                 
                 
