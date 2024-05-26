aa1ddcb4e2b000e9ca615b93c53bd3111712a3a2
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-05-26 23:13:52 +0100

    Time: 706 ms (55.27%), Space: 20.7 MB (72.10%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,52 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sort the array
         nums.sort()
         triplets = []
-        # loop through the array and for each iteration find two other elements with a sum of arr[i]
-        for i in range(len(nums)):
-            left = i+1
-            right = len(nums)-1
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                i += 1
+                continue
+            left, right = i + 1, len(nums)-1
             while left < right:
-                current_sum = nums[left] + nums[right]
-                if current_sum == -nums[i]:
-                    ans = [nums[i],nums[left],nums[right]]
-                    if ans not in triplets:
-                        triplets.append(ans)
+                cur_sum = nums[left] + nums[right] + nums[i]
+                if cur_sum < 0:
                     left += 1
-                    right -=1
-                elif -nums[i] > current_sum:
-                    left += 1
-                else:
+                elif cur_sum > 0:
                     right -= 1
+                else:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                        continue
         return triplets
-    
-#     class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         # [-1,0,1,2,-1,-4] --> [-4, -1, -1, 0 , 1, 2]
-#         # i = 0
-
-#                     # k = 1
-        
-#         result = []
-#         nums.sort()
-        
-        
-#         for i in range(len(nums)):
-#             j = len(nums) - 1
-#             k = i + 1
-#             while k < j:
-#                 sum_ = nums[i] + nums[j] + nums[k]
-#                 if sum_ == 0: 
-#                     ans = [nums[i], nums[j], nums[k]]
-#                     if ans not in result:
-#                         result.append(ans)
-#                     k += 1
-#                     j -= 1
-#                 elif sum_ < 0:
-#                     k += 1
-#                 elif sum_ > 0:
-#                     j -= 1
-#         return result
-                
-        
