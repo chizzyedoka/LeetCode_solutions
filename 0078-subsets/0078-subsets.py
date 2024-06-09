182f2780d3b4edd96417333eee7fd595ef47d627
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-09 14:59:09 +0100

    Time: 34 ms (76.92%), Space: 16.9 MB (7.58%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,23 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         subsets = []
+#         subsets.append([])
+#         for char in nums:
+#             n = len(subsets)
+#             for i in range(n):
+#                 myset = list(subsets[i])
+#                 myset.append(char)
+#                 subsets.append(myset)
+#         return subsets
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        subsets = []
-        subsets.append([])
-        for char in nums:
-            n = len(subsets)
-            for i in range(n):
-                myset = list(subsets[i])
-                myset.append(char)
-                subsets.append(myset)
-        return subsets
+        res = [[]]  # Start with the empty subset
+        
+        for num in nums:
+            new_subsets = []
+            for subset in res:
+                new_subsets.append(subset + [num])  # Add current num to each subset
+            res += new_subsets  # Add the new subsets to the result list
+        
+        return res
