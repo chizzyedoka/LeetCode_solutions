a42045cb5aadee38aa2add6be8edcd36cc0735f5
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-09 10:12:28 +0100

    Time: 46 ms (37.55%), Space: 16.7 MB (98.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,47 @@
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         def find_pivot(nums):
-#             left, right = 0, len(nums)-1
-#             while left <= right:
-#                 mid = (left+right)//2
-#                 if nums[mid+1] < nums[mid]:
-#                     return nums[mid]
-#                 elif 
-                
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
-        while left<=right:
-            mid = left+(right-left)//2
-            if nums[mid] == target:
-                return mid
-            if nums[mid]<nums[right]:
-                if nums[mid]<target<=nums[right]:
-                    left=mid+1
+        def find_pivot(nums):
+            left, right = 0, len(nums)-1
+            while left < right:
+                mid = (left+right)//2
+                if nums[mid] > nums[right]:
+                    left = mid + 1
                 else:
-                    right=mid-1
-            else:
-                if nums[left]<=target<nums[mid]:
-                    right=mid-1
+                    right = mid
+            return left
+        
+        def binary_search(nums, target, left, right):
+            while left <= right:
+                mid = (left + right)// 2
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    left = mid+1
-        return -1
+                    return mid
+            return -1
+        
+        pivot = find_pivot(nums) # smallest number in arr
+        found_index = binary_search(nums, target, 0, pivot)
+        if found_index == -1:
+            found_index = binary_search(nums, target, pivot, len(nums)-1)
+        return found_index
+                
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         left, right = 0, len(nums)-1
+#         while left<=right:
+#             mid = left+(right-left)//2
+#             if nums[mid] == target:
+#                 return mid
+#             if nums[mid]<nums[right]:
+#                 if nums[mid]<target<=nums[right]:
+#                     left=mid+1
+#                 else:
+#                     right=mid-1
+#             else:
+#                 if nums[left]<=target<nums[mid]:
+#                     right=mid-1
+#                 else:
+#                     left = mid+1
+#         return -1
