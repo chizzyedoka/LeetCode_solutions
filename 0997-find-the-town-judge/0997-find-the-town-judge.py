e58df7152dc3e23f98e5667d8569500f39fc1d2a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-13 08:18:04 +0100

    Time: 608 ms (33.30%), Space: 21.5 MB (89.68%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,39 +1,43 @@
-# class Solution:
-#     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-#         if not trust:
-#             return n
-#         graph = {}
-#         possible = -1
-#         for u, v in trust:
-#             if u not in graph:
-#                 graph[u] = []
-#             graph[u].append(v)
-#         for i in range(n):
-#             if i+1 not in graph.keys():
-#                 possible = i+1
-#         for people in graph.values():
-#             if possible not in people:
-#                 possible = -1
-#         return possible 
-
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if n == 1:
-            return 1
+        if len(trust)==0 and n>1:
+            return -1
+        graph = {}
+        possible = -1
+        for u, v in trust:
+            if u not in graph:
+                graph[u] = []
+            graph[u].append(v)
+        for i in range(n):
+            if (i+1) not in graph.keys():
+                possible = i+1
+        if possible != -1:
+            for k in graph:
+                if possible not in graph[k]:
+                    possible = -1
+        return possible
         
-        # Initialize trust counts
-        trust_count = [0] * (n + 1)
-        trusted_by_count = [0] * (n + 1)
+            
+       
+
+ # class Solution:
+#     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+#         if n == 1:
+#             return 1
         
-        # Populate the trust arrays
-        for a, b in trust:
-            trust_count[b] += 1
-            trusted_by_count[a] += 1
+#         # Initialize trust counts
+#         trust_count = [0] * (n + 1)
+#         trusted_by_count = [0] * (n + 1)
         
-        # Find the judge
-        for i in range(1, n + 1):
-            if trust_count[i] == n - 1 and trusted_by_count[i] == 0:
-                return i
+#         # Populate the trust arrays
+#         for a, b in trust:
+#             trust_count[b] += 1
+#             trusted_by_count[a] += 1
         
-        return -1
+#         # Find the judge
+#         for i in range(1, n + 1):
+#             if trust_count[i] == n - 1 and trusted_by_count[i] == 0:
+#                 return i
+        
+#         return -1
 
