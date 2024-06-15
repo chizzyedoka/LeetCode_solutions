6d079cafb158c125eb30de7c14dacb3549719c7a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-15 01:03:44 +0100

    Time: 41 ms (16.17%), Space: 16.4 MB (95.71%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def getRow(self, rowIndex):
-        """
-        :type rowIndex: int
-        :rtype: List[int]
-        """
-        row = [1]
-        for _ in range(rowIndex):
-            row = [x + y for x, y in zip([0]+row, row+[0])]
+        row = [1]*(rowIndex+1)
+        if rowIndex == 0:
+            return row
+        prev_row = self.getRow(rowIndex-1)
+        for i in range(1, len(row)-1):
+            row[i] = prev_row[i-1] + prev_row[i]
         return row
+       
