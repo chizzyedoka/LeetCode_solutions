e25fc019c2e828dc4578e204b297651168fb1b27
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-17 07:10:25 +0100

    Time: 52 ms (5.35%), Space: 16.8 MB (8.47%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,38 +1,38 @@
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         def backtrack(tempList: List[int]):
-#             if len(tempList) == len(nums):
-#                 res.append(tempList[:])
-#             else:
-#                 for num in nums:
-#                     if num in tempList:
-#                         continue
-#                     tempList.append(num)
-#                     backtrack(tempList)
-#                     tempList.pop()
-        
-#         res = []
-#         backtrack([])
-#         return res
-
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(start: int):
-            # If we've reached the end of the array, add a copy of nums to result
-            if start == len(nums):
-                res.append(nums[:])
-            for i in range(start, len(nums)):
-                # Swap the current element with the start element
-                nums[start], nums[i] = nums[i], nums[start]
-                # Recurse with the next position as the new start
-                backtrack(start + 1)
-                # Backtrack by swapping the elements back
-                nums[start], nums[i] = nums[i], nums[start]
-        
-        res = []
-        backtrack(0)
+        res, sol = [], []
+        n = len(nums)
+        def backtrack():
+            if len(sol) == n:
+                res.append(sol[:])
+                return
+            for i in range(n):
+                if nums[i] not in sol:
+                    sol.append(nums[i])
+                    backtrack()
+                    sol.pop()
+        backtrack()
         return res
+            
+
+
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         def backtrack(start):
+#             # If we've reached the end of the array, add a copy of nums to result
+#             if start == len(nums):
+#                 res.append(nums[:])
+#             for i in range(start, len(nums)):
+#                 # Swap the current element with the start element
+#                 nums[start], nums[i] = nums[i], nums[start]
+#                 # Recurse with the next position as the new start
+#                 backtrack(start + 1)
+#                 # Backtrack by swapping the elements back
+#                 nums[start], nums[i] = nums[i], nums[start]
+
+#         backtrack(0)
+#         return res
 
 
 
