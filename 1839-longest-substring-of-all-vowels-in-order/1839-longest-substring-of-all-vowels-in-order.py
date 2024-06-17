fbfdd799bf8e65f397728637413a3e574df2f1c9
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-17 23:55:31 +0100

    Time: 933 ms (10.08%), Space: 22.6 MB (29.46%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestBeautifulSubstring(self, word: str) -> int:
+        window_start, res = 0, 0
+        hashmap = {}
+        queue = deque()
+        for window_end in range(len(word)):
+            char = word[window_end]
+            if char not in hashmap:
+                hashmap[char] = 0
+            hashmap[char] += 1
+            while queue and queue[-1] > char and window_start < window_end:
+                removed = queue.popleft()
+                hashmap[removed] -= 1
+                if hashmap[removed] == 0:
+                    del hashmap[removed]
+                window_start += 1
+            queue.append(char)
+            if len(hashmap) == 5:
+                res = max(res,window_end - window_start + 1)       
+        return res
+
+
+
