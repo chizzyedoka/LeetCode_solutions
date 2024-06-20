027b30b7961606f4124901b7adf09e1011cc02a8
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-20 10:48:53 +0100

    Time: 47 ms (47.39%), Space: 17 MB (62.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,12 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
         rows, cols = len(matrix), len(matrix[0])
-        r, c = 0, cols-1
-        while r < rows and c>=0:
-            if matrix[r][c]==target:
+        left , right = 0, rows * cols-1
+        
+        while left <= right:
+            mid = (left+right)//2
+            mid_value = matrix[mid // cols][mid%cols]
+            if mid_value == target:
                 return True
-            elif matrix[r][c]>target:
-                c-=1 # move left
+            elif mid_value < target:
+                left = mid + 1
             else:
-                r+= 1 # move down
-        return None
+                right = mid - 1
+        return False
+
+
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         rows, cols = len(matrix), len(matrix[0])
+#         r, c = 0, cols-1
+#         while r < rows and c>=0:
+#             if matrix[r][c]==target:
+#                 return True
+#             elif matrix[r][c]>target:
+#                 c-=1 # move left
+#             else:
+#                 r+= 1 # move down
+#         return None
