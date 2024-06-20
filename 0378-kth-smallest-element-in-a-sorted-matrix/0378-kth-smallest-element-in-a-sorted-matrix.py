a6ae6639aa138c6aa050f3b0400540f607695c35
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-21 00:10:49 +0100

    Time: 132 ms (92.96%), Space: 21.1 MB (57.60%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,35 +1,57 @@
 class Solution:
-    def kthSmallest(self, matrix, k):
-        rows = len(matrix)
-        cols = len(matrix[0])
+    def kthSmallest(self,matrix, k):
+        def count_less_equal(x):
+            count, j = 0, len(matrix[0]) - 1
+            for i in range(len(matrix)):
+                while j >= 0 and matrix[i][j] > x:
+                    j -= 1
+                count += (j + 1)
+            return count
 
-        lo = matrix[0][0]
-        hi = matrix[rows - 1][cols - 1]
+        n = len(matrix)
+        low, high = matrix[0][0], matrix[n - 1][n - 1]
 
-        while lo <= hi:
-            mid = lo + (hi - lo) // 2
-            count = 0
-            max_num = lo
-
-            for r in range(rows):
-                c = cols - 1
-                while c >= 0 and matrix[r][c] > mid:
-                    c -= 1
-
-                if c >= 0:
-                    count += (c + 1)
-                    max_num = max(max_num, matrix[r][c])
-                else:
-                    break
-
-            if count == k:
-                return max_num
-            elif count < k:
-                lo = mid + 1
+        while low < high:
+            mid = (low + high) // 2
+            if count_less_equal(mid) < k:
+                low = mid + 1
             else:
-                hi = mid - 1
+                high = mid
 
-        return lo
+        return low
+
+# class Solution:
+#     def kthSmallest(self, matrix, k):
+#         rows = len(matrix)
+#         cols = len(matrix[0])
+
+#         lo = matrix[0][0]
+#         hi = matrix[rows - 1][cols - 1]
+
+#         while lo <= hi:
+#             mid = lo + (hi - lo) // 2
+#             count = 0
+#             max_num = lo
+
+#             for r in range(rows):
+#                 c = cols - 1
+#                 while c >= 0 and matrix[r][c] > mid:
+#                     c -= 1
+
+#                 if c >= 0:
+#                     count += (c + 1)
+#                     max_num = max(max_num, matrix[r][c])
+#                 else:
+#                     break
+
+#             if count == k:
+#                 return max_num
+#             elif count < k:
+#                 lo = mid + 1
+#             else:
+#                 hi = mid - 1
+
+#         return lo
 # class Solution:
 #     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
 #         minHeap = []
