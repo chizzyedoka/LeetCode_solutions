76c8c238ff83cc9cb28433174df7321141dac519
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-23 01:19:56 +0100

    Time: 56 ms (33.47%), Space: 16.9 MB (29.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,30 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        def greater(n, nums):
-            seen = False
-            for num in nums:
-                if n == num:
-                    seen = True
-                if num > n and seen:
-                    return num
-            return -1
+        hashmap = {}
+        stack = [nums2[0]]
         res = []
+        for i in range(1, len(nums2)):
+            while stack and nums2[i] > stack[-1]:
+                hashmap[stack[-1]] = nums2[i]
+                stack.pop()
+            stack.append(nums2[i])
         for num in nums1:
-            res.append(greater(num, nums2))
+            res.append(hashmap.get(num,-1))
         return res
+
+
+# class Solution:
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         def greater(n, nums):
+#             seen = False
+#             for num in nums:
+#                 if n == num:
+#                     seen = True
+#                 if num > n and seen:
+#                     return num
+#             return -1
+#         res = []
+#         for num in nums1:
+#             res.append(greater(num, nums2))
+#         return res
             
