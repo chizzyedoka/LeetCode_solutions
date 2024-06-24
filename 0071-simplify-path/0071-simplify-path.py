53d089cfb55ea23bd84939c172c4b0388a9fd327
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-24 10:34:41 +0100

    Time: 39 ms (45.24%), Space: 16.5 MB (69.38%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,63 +1,35 @@
-class Solution:
-    def simplifyPath(self, path: str) -> str:
-        # Split the path by '/' to handle each component
-        components = path.split('/')
-        stack = []
-
-        for component in components:
-            if component == '' or component == '.':
-                # Ignore empty components and current directory references
-                continue
-            elif component == '..':
-                # Pop from the stack if there is a previous directory
-                if stack:
-                    stack.pop()
-            else:
-                # Push the directory name onto the stack
-                stack.append(component)
-
-        # Join the stack elements with '/' to form the simplified path
-        return '/' + '/'.join(stack)
-
-# Example usage:
-solution = Solution()
-path = "/a/./b/../../c/"
-print(solution.simplifyPath(path))  # Output: "/c"
-
-
 # class Solution:
 #     def simplifyPath(self, path: str) -> str:
+#         # Split the path by '/' to handle each component
+#         components = path.split('/')
 #         stack = []
-#         res = ""
-        
-#         i = 0
-#         while i < len(path):
-#             if path[i] == '/':
-#                 i += 1
+
+#         for component in components:
+#             if component == '' or component == '.':
+#                 # Ignore empty components and current directory references
 #                 continue
-            
-#             temp = ""
-#             # Iterate till we traverse the whole string and encounter the next '/'
-#             while i < len(path) and path[i] != '/':
-#                 temp += path[i]
-#                 i += 1
-            
-#             if temp == ".":
-#                 continue
-#             elif temp == "..":
+#             elif component == '..':
+#                 # Pop from the stack if there is a previous directory
 #                 if stack:
 #                     stack.pop()
 #             else:
-#                 stack.append(temp)
-        
-#         # Adding all the stack elements to result
-#         while stack:
-#             res = "/" + stack.pop() + res
-        
-#         # If no directory or file is present
-#         if not res:
-#             return "/"
-        
-#         return res
+#                 # Push the directory name onto the stack
+#                 stack.append(component)
+
+#         # Join the stack elements with '/' to form the simplified path
+#         return '/' + '/'.join(stack)
+
+
+
+class Solution:
+    def simplifyPath(self, path):
+        stack = []
+        for elem in path.split("/"):
+            if stack and elem == "..":
+                stack.pop()
+            elif elem not in [".", "", ".."]:
+                stack.append(elem)
+                
+        return "/" + "/".join(stack)
 
 
