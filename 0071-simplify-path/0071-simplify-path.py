62743651c4ace19d03298e3f7e70a31dbefe22c1
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-24 10:33:28 +0100

    Time: 31 ms (89.83%), Space: 16.7 MB (27.48%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,36 +1,63 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        # Split the path by '/' to handle each component
+        components = path.split('/')
         stack = []
-        res = ""
-        
-        i = 0
-        while i < len(path):
-            if path[i] == '/':
-                i += 1
+
+        for component in components:
+            if component == '' or component == '.':
+                # Ignore empty components and current directory references
                 continue
-            
-            temp = ""
-            # Iterate till we traverse the whole string and encounter the next '/'
-            while i < len(path) and path[i] != '/':
-                temp += path[i]
-                i += 1
-            
-            if temp == ".":
-                continue
-            elif temp == "..":
+            elif component == '..':
+                # Pop from the stack if there is a previous directory
                 if stack:
                     stack.pop()
             else:
-                stack.append(temp)
+                # Push the directory name onto the stack
+                stack.append(component)
+
+        # Join the stack elements with '/' to form the simplified path
+        return '/' + '/'.join(stack)
+
+# Example usage:
+solution = Solution()
+path = "/a/./b/../../c/"
+print(solution.simplifyPath(path))  # Output: "/c"
+
+
+# class Solution:
+#     def simplifyPath(self, path: str) -> str:
+#         stack = []
+#         res = ""
         
-        # Adding all the stack elements to result
-        while stack:
-            res = "/" + stack.pop() + res
+#         i = 0
+#         while i < len(path):
+#             if path[i] == '/':
+#                 i += 1
+#                 continue
+            
+#             temp = ""
+#             # Iterate till we traverse the whole string and encounter the next '/'
+#             while i < len(path) and path[i] != '/':
+#                 temp += path[i]
+#                 i += 1
+            
+#             if temp == ".":
+#                 continue
+#             elif temp == "..":
+#                 if stack:
+#                     stack.pop()
+#             else:
+#                 stack.append(temp)
         
-        # If no directory or file is present
-        if not res:
-            return "/"
+#         # Adding all the stack elements to result
+#         while stack:
+#             res = "/" + stack.pop() + res
         
-        return res
+#         # If no directory or file is present
+#         if not res:
+#             return "/"
+        
+#         return res
 
 
