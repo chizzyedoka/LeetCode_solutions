fd140880163f95d41339a86dd9265e16751d0255
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-06-28 00:37:32 +0100

    Time: 205 ms (74.72%), Space: 17.9 MB (90.37%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,72 +1,44 @@
-# class Solution:
-#     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-#         pacific_q = queue()
-#         pacific_seen = set()
-        
-#         atlantic_q = queue()
-#         atlantic_seen = set()
-        
-#         rows, cols = len(heights), len(heights[0])
-        
-#         for j in range(cols):
-#             pacific_q.append((0,j))
-#             pacific_seen.add((0,j))
-            
-#         for i in range(1,rows):
-#             pacific_q.append((i,0))
-#             pacific_seen.add((i,0))
-            
-#         for i in range(1,rows):
-#             atlantic_q.append((i,cols-1))
-#             atlantic_seen.append((i, cols-1))
-            
-#         for j in range(cols-1):
-#             atlantic_q.append((rows-1,j))
-#             atlantic_seen.append((rows-1,j))
-            
-#         def get_coords(q,seen):
-#             coords = set()
-from collections import deque
-
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        p_que = deque()
-        p_seen = set()
+        pacific_q = deque()
+        pacific_seen = set()
         
-        a_que = deque()
-        a_seen = set()
+        atlantic_q = deque()
+        atlantic_seen = set()
         
-        m, n = len(heights), len(heights[0])
-
-        for j in range(n):
-            p_que.append((0, j))
-            p_seen.add((0, j))
+        rows, cols = len(heights), len(heights[0])
+        
+        for j in range(cols):
+            pacific_q.append((0,j))
+            pacific_seen.add((0,j))
             
-        for i in range(1, m):
-            p_que.append((i, 0))
-            p_seen.add((i, 0))
+        for i in range(1,rows):
+            pacific_q.append((i,0))
+            pacific_seen.add((i,0))
             
-        for i in range(m):
-            a_que.append((i, n - 1))
-            a_seen.add((i, n - 1))
+        for i in range(rows):
+            atlantic_q.append((i,cols-1))
+            atlantic_seen.add((i, cols-1))
             
-        for j in range(n - 1):
-            a_que.append((m - 1, j))
-            a_seen.add((m - 1, j))
-
-        def get_coords(que, seen):
-            coords = set()
-            while que:
-                i, j = que.popleft()
-                for i_off, j_off in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    r, c = i + i_off, j + j_off
-                    if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r, c) not in seen:
-                        seen.add((r, c))
-                        que.append((r, c))
+        for j in range(cols-1):
+            atlantic_q.append((rows-1,j))
+            atlantic_seen.add((rows-1,j))
             
-        get_coords(p_que, p_seen)
-        get_coords(a_que, a_seen)
-        return list(p_seen.intersection(a_seen))
-
-# Time Complexity: O(m*n)
-# Space Complexity: O(m*n)
+        def get_coords(q,seen):
+            while q:
+                (i,j) = q.popleft()
+                for (i_off,j_off) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    r,c = i+i_off, j+j_off
+                    row_inbounds = 0 <= r < rows
+                    col_inbounds = 0 <= c < cols
+                    if row_inbounds and col_inbounds and heights[r][c] >= heights[i][j] and (r,c) not in seen:
+                        q.append((r,c))
+                        seen.add((r,c))
+        
+        get_coords(pacific_q, pacific_seen)
+        get_coords(atlantic_q, atlantic_seen)
+        return list(pacific_seen.intersection(atlantic_seen))
+        
+        
+        
+            
