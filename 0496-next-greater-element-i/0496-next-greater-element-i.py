acf1fc6ffc9b70bdd33319c86fbf4a9e4ce58a3b
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-07-01 11:07:16 +0100

    Time: 44 ms (82.95%), Space: 16.8 MB (67.00%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,30 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hashmap = {}
-        stack = [nums2[0]]
-        res = []
-        for i in range(1, len(nums2)):
-            while stack and nums2[i] > stack[-1]:
-                hashmap[stack[-1]] = nums2[i]
+        next_greater = {}
+        stack = []
+        for num in reversed(nums2):
+            while stack and stack[-1] <= num:
                 stack.pop()
-            stack.append(nums2[i])
-        for num in nums1:
-            res.append(hashmap.get(num,-1))
-        return res
-
-
-# class Solution:
-#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-#         def greater(n, nums):
-#             seen = False
-#             for num in nums:
-#                 if n == num:
-#                     seen = True
-#                 if num > n and seen:
-#                     return num
-#             return -1
-#         res = []
-#         for num in nums1:
-#             res.append(greater(num, nums2))
-#         return res
+            if stack:
+                next_greater[num] = stack[-1]
+            else:
+                next_greater[num] = -1
+            stack.append(num)
+        return [next_greater[x] for x in nums1]
+    
             
