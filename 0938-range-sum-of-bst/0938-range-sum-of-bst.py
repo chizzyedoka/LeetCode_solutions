da225a1e5b94a122c1df8d7111bfba30c932252a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-07-23 09:33:46 +0100

    Time: 108 ms (73.77%), Space: 23.7 MB (28.53%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -5,21 +5,41 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    from collections import deque
+    # DFS approach
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        if not root:
-            return 0
-        q = deque()
-        q.append(root)
-        _sum = 0
-        while q:
-            level_size = len(q)
-            for _ in range(level_size):
-                node = q.popleft()
-                if low <= node.val <= high:
-                    _sum += node.val
-                if node.left and node.val > low:
-                    q.append(node.left)
-                if node.right and node.val < high:
-                    q.append(node.right)
-        return _sum
+
+        def dfs(node):
+            if not node:
+                return 0
+            total = 0
+            if low <= node.val <= high:
+                total += node.val
+            if node.left and node.val > low:
+                total += dfs(node.left)
+            if node.right and node.val < high:
+                total += dfs(node.right)
+            return total
+                
+        return dfs(root)
+        
+    
+    
+    # BFS approach
+    # from collections import deque
+    # def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    #     if not root:
+    #         return 0
+    #     q = deque()
+    #     q.append(root)
+    #     _sum = 0
+    #     while q:
+    #         level_size = len(q)
+    #         for _ in range(level_size):
+    #             node = q.popleft()
+    #             if low <= node.val <= high:
+    #                 _sum += node.val
+    #             if node.left and node.val > low:
+    #                 q.append(node.left)
+    #             if node.right and node.val < high:
+    #                 q.append(node.right)
+    #     return _sum
