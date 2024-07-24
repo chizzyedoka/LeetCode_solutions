76a6cbc69282de64e77611befd792dea548cf756
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-07-25 00:47:10 +0100

    Time: 184 ms (44.72%), Space: 20.5 MB (31.79%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -6,16 +6,41 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        tree_list = []
-        def inorder(node):
+        def inorder(node,lst):
             if not node:
                 return
-            tree_list.append(node.val)
-            inorder(node.left)
-            inorder(node.right)
-        inorder(root1)
-        inorder(root2)
-        tree_list.sort()
-        return tree_list
+            inorder(node.left, lst)
+            lst.append(node.val)
+            inorder(node.right, lst)
+        lst1, lst2 = [], []
+        inorder(root1, lst1)
+        inorder(root2, lst2)
+        i, j = 0, 0
+        res = []
+        while i < len(lst1) and j < len(lst2):
+            if lst1[i] <= lst2[j]:
+                res.append(lst1[i])
+                i += 1
+            else:
+                res.append(lst2[j])
+                j += 1
+        if i < len(lst1):
+            res += lst1[i:]
+        elif j < len(lst2):
+            res += lst2[j:]
+        return res
+    # brute force O[N+M log(n+m)]
+    # def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+    #     tree_list = []
+    #     def inorder(node):
+    #         if not node:
+    #             return
+    #         inorder(node.left)
+    #         tree_list.append(node.val)
+    #         inorder(node.right)
+    #     inorder(root1)
+    #     inorder(root2)
+    #     tree_list.sort()
+    #     return tree_list
             
         
