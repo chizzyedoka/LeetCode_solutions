848795620e67859276d29f86934ea64432275526
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-07-25 01:38:14 +0100

    Time: 38 ms (38.57%), Space: 16.5 MB (55.24%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -4,33 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        def dfs(node, tree_lst):
+        def reverse_inorder(node, acc_sum):
             if not node:
-                return
-            dfs(node.left, tree_lst)
-            tree_lst.append(node.val)
-            dfs(node.right, tree_lst)
+                return acc_sum
+            acc_sum = reverse_inorder(node.right, acc_sum)
+            acc_sum += node.val
+            node.val = acc_sum
+            acc_sum = reverse_inorder(node.left, acc_sum)
+            return acc_sum
         
-        def replace_values(node):
-            if not node:
-                return
-            replace_values(node.left)
-            replace_values(node.right)
-            node_sum = 0
-            for i in tree_lst:
-                if i > node.val:
-                    node_sum += i
-                else:
-                    break
-            node.val += node_sum
-            
-        tree_lst = []
-        dfs(root, tree_lst)
-        tree_lst.reverse()
-        replace_values(root)
+        reverse_inorder(root, 0)
         return root
+
+# class Solution:
+#     def bstToGst(self, root: TreeNode) -> TreeNode:
+#         def dfs(node, tree_lst):
+#             if not node:
+#                 return
+#             dfs(node.left, tree_lst)
+#             tree_lst.append(node.val)
+#             dfs(node.right, tree_lst)
+        
+#         def replace_values(node):
+#             if not node:
+#                 return
+#             replace_values(node.left)
+#             replace_values(node.right)
+#             node_sum = 0
+#             for i in tree_lst:
+#                 if i > node.val:
+#                     node_sum += i
+#                 else:
+#                     break
+#             node.val += node_sum
+            
+#         tree_lst = []
+#         dfs(root, tree_lst)
+#         tree_lst.reverse()
+#         replace_values(root)
+#         return root
         
         
             
