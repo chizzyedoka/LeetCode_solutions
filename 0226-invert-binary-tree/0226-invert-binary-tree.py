07196afb59ed5c10ae137e0ad6e594f717a2a997
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-07-26 06:40:38 +0100

    Time: 46 ms (5.69%), Space: 16.4 MB (91.80%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -4,37 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-#         if not root:
-#             return
-#         q = deque([root])
-#         while q:
-#             size = len(q)
-#             node = q.popleft()
-#             node.left, node.right = node.right, node.left
-#             for _ in range(size):
-#                 if node.left:
-#                     q.append(node.left)
-#                 if node.right:
-#                     q.append(node.right)
-#         return root
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return None
-        
+            return
         q = deque([root])
         while q:
-            node = q.popleft()
-            
-            # Swap the left and right children
-            node.left, node.right = node.right, node.left
-            
-            # Add the children to the queue if they are not None
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                node.left, node.right = node.right, node.left
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
         return root
+    
+# class Solution:
+#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if not root:
+#             return None
+        
+#         q = deque([root])
+#         while q:
+#             node = q.popleft()
+            
+#             # Swap the left and right children
+#             node.left, node.right = node.right, node.left
+            
+#             # Add the children to the queue if they are not None
+#             if node.left:
+#                 q.append(node.left)
+#             if node.right:
+#                 q.append(node.right)
+        
+#         return root
