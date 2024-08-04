549968964ad6cf232f8d2d12f2ecce18e5e8e10a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-08-04 07:46:25 +0100

    Time: 59 ms (58.87%), Space: 18.5 MB (41.52%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
@@ -6,22 +6,37 @@
 #         self.right = right
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def visit1(root):
-            if root:
-                visit1(root.left)
-                vals.append(root.val)
-                visit1(root.right)
-        vals = []
-        visit1(root)
-
-        self.s = 0
-        def visit2(root):
-            if root:
-                visit2(root.right)
-                self.s += vals.pop()
-                root.val = self.s
-                visit2(root.left)
-        visit2(root)
+        self.curSum = 0
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.right)
+            self.curSum += node.val
+            node.val = self.curSum
+            dfs(node.left)
+        dfs(root)
         return root
+        
+        
+        
+# class Solution:
+#     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         def visit1(root):
+#             if root:
+#                 visit1(root.left)
+#                 vals.append(root.val)
+#                 visit1(root.right)
+#         vals = []
+#         visit1(root)
+
+#         self.s = 0
+#         def visit2(root):
+#             if root:
+#                 visit2(root.right)
+#                 self.s += vals.pop()
+#                 root.val = self.s
+#                 visit2(root.left)
+#         visit2(root)
+#         return root
 
         
