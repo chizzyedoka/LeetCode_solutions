188077c3e3dd4ef2b1449f9a36538ea4e8725106
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-08-04 08:48:35 +0100

    Time: 185 ms (13.44%), Space: 17.8 MB (58.43%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,26 +1,41 @@
-import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = {node:[] for node in range(n)}
-        for u,v,w in flights:
-            graph[u].append((v,w))
-       
-        shortest = {}
-        minHeap = [(0, src, 0)]  
-        
-        while minHeap:
-            w1, n1, stops = heapq.heappop(minHeap)
-            if n1 == dst:
-                return w1
-            
-            if stops > k:
-                continue
-                
-            if (n1, stops) in shortest and shortest[(n1, stops)] <= w1:
-                continue
-                
-            shortest[(n1, stops)] = w1
-            for n2, w2 in graph[n1]:
-                if n2 not in shortest:
-                    heapq.heappush(minHeap, (w1+w2, n2, stops+1) )
+        distances = [float('inf')] * n
+        distances[src] = 0
+        for i in range(k+1):
+            temp_distances = distances[:]
+            for u, v, w in flights:
+                if distances[u] != float('inf') and distances[u] + w < temp_distances[v]:
+                    temp_distances[v] = distances[u] + w
+            distances = temp_distances
+        if distances[dst] != float('inf'):
+            return distances[dst]
         return -1
+
+# dijkstra's algorithm
+# import heapq
+# class Solution:
+#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+#         graph = {node:[] for node in range(n)}
+#         for u,v,w in flights:
+#             graph[u].append((v,w))
+       
+#         shortest = {}
+#         minHeap = [(0, src, 0)]  
+        
+#         while minHeap:
+#             w1, n1, stops = heapq.heappop(minHeap)
+#             if n1 == dst:
+#                 return w1
+            
+#             if stops > k:
+#                 continue
+                
+#             if (n1, stops) in shortest and shortest[(n1, stops)] <= w1:
+#                 continue
+                
+#             shortest[(n1, stops)] = w1
+#             for n2, w2 in graph[n1]:
+#                 if n2 not in shortest:
+#                     heapq.heappush(minHeap, (w1+w2, n2, stops+1) )
+#         return -1
