c3b07cd65dad141b6fa8066a30c84d564db2426d
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-08-22 00:23:31 +0100

    Time: 48 ms (7.58%), Space: 16.5 MB (35.21%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -3,35 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         cur = head
-#         last_unduplicated_node = cur
-#         while cur and cur.next:
-#             if cur.val != cur.next.val:
-#                 last_unduplicated_node = cur
-#                 cur = cur.next
-#             else:
-#                 while cur.val == cur.next.val:
-#                     cur = cur.next
-#                 last_unduplicated_node.next = cur.next
-#                 cur = cur.next
-#         return head
-    
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Create a dummy node to handle edge cases where the first node is duplicated
-        dummy = ListNode(0)
-        dummy.next = head
+        dummy = ListNode(0, head)
         cur = dummy
-        
-        while cur.next and cur.next.next:
-            if cur.next.val == cur.next.next.val:
-                # Skip all nodes with the same value
-                duplicate_val = cur.next.val
-                while cur.next and cur.next.val == duplicate_val:
-                    cur.next = cur.next.next
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                cur.next = head.next
             else:
                 cur = cur.next
-        
+            head = head.next
         return dummy.next
+    
+
