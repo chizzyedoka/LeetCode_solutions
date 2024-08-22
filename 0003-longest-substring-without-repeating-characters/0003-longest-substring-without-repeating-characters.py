5c52ae47c2195a26029b8261da830606439fbf42
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-08-22 20:53:32 +0100

    Time: 57 ms (49.93%), Space: 16.6 MB (79.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,47 +1,12 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         n = len(s)
-#         ans = 0
-#         # mp stores the current index of a character
-#         mp = {}
-
-#         i = 0
-#         # try to extend the range [i, j]
-#         for j in range(n):
-#             if s[j] in mp:
-#                 i = max(mp[s[j]], i)
-
-#             ans = max(ans, j - i + 1)
-#             mp[s[j]] = j + 1
-
-#         return ans
-    
-    
-    
-    
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         substring = deque()
-#         max_substring = 0
-#         for char in s:
-#             while char in substring:
-#                 substring.popleft()
-#             substring.append(char)
-#             max_substring = max(max_substring, len(substring))
-#         return max_substring
-
-    
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        window_start, max_length = 0,0
+        start, res = 0, 0
         char_index = {}
-        for window_end in range(len(s)):
-            right_char = s[window_end]
-            if right_char in char_index:
-                window_start = max(window_start, char_index[right_char]+1)
-            char_index[right_char] = window_end
-            max_length = max(max_length, window_end-window_start+1)
-        return max_length
-
-                
-                
+        for end in range(len(s)):
+            char = s[end]
+            if char in char_index:
+                # shrink the window
+                start = max(start, char_index[char] + 1)
+            char_index[char] = end
+            res = max(res, end - start + 1)
+        return res
