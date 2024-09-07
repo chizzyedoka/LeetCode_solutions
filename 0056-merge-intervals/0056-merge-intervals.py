1a05b746285763a316b92b8efd0ffe965dc893ef
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-09-07 16:26:23 +0100

    Time: 115 ms (90.80%), Space: 21.2 MB (27.51%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,34 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) < 2:
-            return intervals
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+#         if len(intervals) < 2:
+#             return intervals
         
+#         intervals.sort(key=lambda x: x[0])
+#         output = [intervals[0]]
+#         for start,end in intervals[1:]:
+#             lastEnd = output[-1][1]
+#             if start <= lastEnd:
+#                 output[-1][1] = max(lastEnd,end)
+#             else:
+#                 output.append([start,end])
+#         return output
+    def merge(self,intervals):
         intervals.sort(key=lambda x: x[0])
-        output = [intervals[0]]
-        for start,end in intervals[1:]:
-            lastEnd = output[-1][1]
-            if start <= lastEnd:
-                output[-1][1] = max(lastEnd,end)
+
+        res = []
+        prev_start, prev_end = intervals[0]
+
+        for i in range(1, len(intervals)):
+            start, end = intervals[i]
+
+
+            if start <= prev_end:
+                prev_end = max(prev_end, end)
             else:
-                output.append([start,end])
-        return output
+                res.append([prev_start, prev_end])
+                prev_start, prev_end = start, end
+
+
+        res.append([prev_start, prev_end])
+
+        return res
