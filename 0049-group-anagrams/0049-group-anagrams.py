b9b31c91b8f0055f685e5bfb73309fe7da489edd
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-09-10 11:12:48 +0100

    Time: 97 ms (28.11%), Space: 22.4 MB (10.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         map = {}
-        count = [0] * 26
         for word in strs:
             count = [0] * 26
             for char in word:
