2fedcabd77117176b449a76d807cf759359c4a80
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-09-10 11:12:36 +0100

    Time: 104 ms (13.74%), Space: 22.4 MB (10.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,29 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+        count = [0] * 26
+        for word in strs:
+            count = [0] * 26
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+            count_tuple = tuple(count)
+            if count_tuple not in map:
+                map[count_tuple] = []
+            map[count_tuple].append(word)
+        return map.values()
+
+    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    #     map = {}
+    #     for word in strs:
+    #         if tuple(sorted(word)) not in map:
+    #             map[tuple(sorted(word))] = []
+    #         map[tuple(sorted(word))].append(word)
+    #     return map.values()
+            
+            
+
+
+
+        
+
+        
