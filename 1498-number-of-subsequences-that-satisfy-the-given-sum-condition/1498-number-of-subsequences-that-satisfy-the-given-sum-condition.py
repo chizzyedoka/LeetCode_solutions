a9fd3c54a81e313d124dc50a0fab92736243ec98
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-11-25 22:00:19 +0100

    Time: 7290 ms (5.03%), Space: 27.6 MB (48.24%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -3,11 +3,17 @@ class Solution:
         MOD = 10**9 + 7
         nums.sort()
         res = 0
-        l, r = 0, len(nums)-1
-        while l <= r:
-            if nums[l] + nums[r] > target:
-                r -= 1
-            else:
-                res += (2**(r-l))
-                l += 1
+        def binSearch(l, r , t):
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] <= t:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return r
+                    
+        for left in range(len(nums)):
+            right = binSearch(left, len(nums)-1 , target - nums[left]) 
+            if right >= left:
+                res += (2**(right-left)) % MOD
         return res % MOD
