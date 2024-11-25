f2105cfcebbc5588ceb4614c2616a8c6e0c6c221
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2024-11-25 09:06:55 +0100

    Time: 0 ms (100.00%), Space: 16.9 MB (57.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,47 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def find_pivot(nums):
-            left, right = 0, len(nums)-1
-            while left < right:
-                mid = (left+right)//2
-                if nums[mid] > nums[right]:
-                    left = mid + 1
+        def getPivot(nums): # largets element in array
+            l, r = 0, len(nums)-1
+            while l < r:
+                mid = (l + r) // 2
+                if nums[mid] > nums[r]:
+                     l = mid + 1
                 else:
-                    right = mid
-            return left
+                    r = mid
+            return r 
         
-        def binary_search(nums, target, left, right):
+        def findTarget(left, right):
             while left <= right:
-                mid = (left + right)// 2
-                if nums[mid] < target:
-                    left = mid + 1
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    return mid
                 elif nums[mid] > target:
                     right = mid - 1
                 else:
-                    return mid
+                    left = mid + 1
             return -1
-        
-        pivot = find_pivot(nums) # smallest number in arr
-        found_index = binary_search(nums, target, 0, pivot)
-        if found_index == -1:
-            found_index = binary_search(nums, target, pivot, len(nums)-1)
-        return found_index
-                
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         left, right = 0, len(nums)-1
-#         while left<=right:
-#             mid = left+(right-left)//2
-#             if nums[mid] == target:
-#                 return mid
-#             if nums[mid]<nums[right]:
-#                 if nums[mid]<target<=nums[right]:
-#                     left=mid+1
-#                 else:
-#                     right=mid-1
-#             else:
-#                 if nums[left]<=target<nums[mid]:
-#                     right=mid-1
-#                 else:
-#                     left = mid+1
-#         return -1
+                    
+        pivotIndex = getPivot(nums)
+        targetIndex = findTarget(0, pivotIndex)
+        if targetIndex == -1:
+            targetIndex = findTarget(pivotIndex, len(nums)-1)
+        return targetIndex
