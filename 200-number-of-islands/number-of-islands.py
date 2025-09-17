c2b161a2f7bac7729df94b1e6bbf6c032c0a9d5f
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-09-17 18:58:32 +0100

    Time: 249 ms (52.96%) | Memory: 25.5 MB (17.86%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -18,8 +18,6 @@ class Solution:
             else:
                 return False
 
-
-
         
         for r in range(m):
             for c in range(n):
@@ -29,3 +27,31 @@ class Solution:
                     
 
         return count
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        seen = set()
+        q = deque()
+
+        LAND = "1"
+        directions = [(-1,0), (1,0), (0, -1), (0, 1)]
+        count = 0
+
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == LAND and (r,c) not in seen:
+                    q.append((r,c))
+                    while q:
+                        i, j = q.popleft()
+                        seen.add((i,j))
+                        for (di, dj) in directions:
+                            new_i, new_j = i + di, j + dj
+                            if (0 <= new_i < m) and (0 <= new_j < n) and grid[new_i][new_j] == LAND and (new_i, new_j) not in seen:
+                                q.append((new_i, new_j)) 
+                                seen.add((new_i, new_j))
+                    count += 1
+
+        return count
+
