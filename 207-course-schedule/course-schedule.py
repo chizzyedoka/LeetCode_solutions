6af587fc2b8403c39b7a2eeaa9675238a3d7e467
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-09-18 19:31:31 +0100

    Time: 5 ms (57.25%) | Memory: 19 MB (72.88%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,39 +1,36 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
-        def graph_formation(courses):
+        
+        def graphFormation(courses):
             graph = defaultdict(list)
             for (u,v) in courses:
                 graph[u].append(v)
             return graph
 
-        
+        def dfs(node):
+            state = states[node]
+            if state == VISITING:
+                return False
+            elif state ==VISITED:
+                return True
+
+            states[node] = VISITING
+            for i in graph[node]:
+                if not dfs(i):
+                    return False
+            
+            states[node] = VISITED
+            return True
+
+
+        graph = graphFormation(prerequisites)
         UNVISITED = 0
         VISITING = 1
         VISITED = 2
         states = [0] * numCourses
-
-        def dfs(node):
-            state = states[node]
-            if state == VISITED:
-                return True
-            elif state == VISITING:
-                return False
-           
-            states[node] = VISITING
-                
-            for neighbor in graph[node]:
-                if not dfs(neighbor):
-                    return False
-            states[node] = VISITED
-            return True
-
-        graph = graph_formation(prerequisites)
-
-
         
-        for node in range(numCourses):
-            if not dfs(node):
+
+        for i in range(numCourses):
+            if not dfs(i):
                 return False
-        
         return True
