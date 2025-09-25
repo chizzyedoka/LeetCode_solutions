7f533890857a364da660491eac8bf1ca9dbcb367
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-09-25 10:42:05 +0100

    Time: 19 ms (36.62%) | Memory: 22.7 MB (11.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -9,3 +9,18 @@ class Solution:
         for word, word_anagram in anagrams.items():
             result.append(word_anagram)
         return result
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = []
+        anagrams = defaultdict(list)
+        for word in strs:
+            bucket = [0] * 26
+            for letter in word:
+                bucket[ord(letter) - ord('a')] += 1
+            tuple_bucket = tuple(bucket)
+            anagrams[tuple_bucket].append(word)
+
+        for word, word_anagram in anagrams.items():
+            result.append(word_anagram)
+        return result
