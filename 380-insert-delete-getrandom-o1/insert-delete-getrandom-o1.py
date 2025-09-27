c058295c5e64bbc8660f2b91bdd022350b538a44
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-09-27 09:33:44 +0100

    Time: 153 ms (39.17%) | Memory: 58 MB (20.17%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -0,0 +1,53 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.itemsList = []
+        self.itemsDict = {}
+
+    def insert(self, val: int) -> bool:
+        if val not in self.itemsDict:
+            self.itemsList.append(val)
+            self.itemsDict[val] = len(self.itemsList) - 1
+            return True
+        return False
+        
+
+    # def remove(self, val: int) -> bool:
+    #     if val not in self.itemsDict:
+    #         return False
+    #     lastElement = self.itemsList[-1]
+    #     indexToRemove = self.itemsDict[val]
+    #     # swap lastElement and value we want to remove
+    #     self.itemsList[indexToRemove], self.itemsList[-1] = lastElement, self.itemsList[indexToRemove]
+    #     self.itemsList.pop()
+    #     del self.itemsDict[val]
+    #     self.itemsDict[lastElement] = indexToRemove
+    #     return True
+    def remove(self, val: int) -> bool:
+        if val not in self.itemsDict:
+            return False
+
+        lastElement = self.itemsList[-1]
+        indexToRemove = self.itemsDict[val]
+
+        # swap val with lastElement (if not same position)
+        self.itemsList[indexToRemove] = lastElement
+        self.itemsList.pop()
+
+        del self.itemsDict[val]
+        if val != lastElement:   # ⚡ only update if different
+            self.itemsDict[lastElement] = indexToRemove
+
+        return True
+
+
+    def getRandom(self) -> int:
+        return random.choice(self.itemsList)
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
