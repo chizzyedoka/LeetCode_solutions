7376e758f0529af35b5baceed79b369c89b7052b
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-09-27 07:38:53 +0100

    Time: 7 ms (45.12%) | Memory: 18.1 MB (15.48%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -7,57 +7,36 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     
         def reverseLL(head):
-            if not head:
-                return
-           
             prev = None
             cur = head
-            nxt = None
             while cur:
-                # store the next node
                 nxt = cur.next
-                # reverse cur's node next pointer
                 cur.next = prev
-                # move pointer's one posiion ahead
                 prev = cur
                 cur = nxt
             return prev
 
+        # Reverse input lists
         l1 = reverseLL(l1)
         l2 = reverseLL(l2)
 
         carry = 0
-        l3 = ListNode()
-        cur = l3
-        while l1 and l2:
-            curSum = l1.val + l2.val + carry
+        dummy = ListNode()
+        cur = dummy
+
+        # Add both lists
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            curSum = val1 + val2 + carry
+
             carry = curSum // 10
             digit = curSum % 10
             cur.next = ListNode(digit)
             cur = cur.next
-            l1 = l1.next
-            l2 = l2.next
 
-        while l1:
-            curSum = l1.val + carry
-            carry = curSum // 10
-            digit = curSum % 10
-            cur.next = ListNode(digit)
-            cur = cur.next
-            l1 = l1.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
 
-        while l2:
-            curSum = l2.val + carry
-            carry = curSum // 10
-            digit = curSum % 10
-            cur.next = ListNode(digit)
-            cur = cur.next
-            l2 = l2.next
-        if carry:
-            cur.next = ListNode(carry)
-            cur = cur.next
-        
-
-
-        return reverseLL(l3.next)
-            
+        # Reverse result back
+        return reverseLL(dummy.next)
