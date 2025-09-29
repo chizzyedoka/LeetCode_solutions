4bf7c10a10bcff1ce53dca1542930bb149b2135f
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-09-29 18:58:11 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (45.65%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,16 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        
         memo = {}
-        def dp(i,j):
-            # base case
-            if i <= 0 or j <= 0:
+        def dp(m,n):
+            if m==1 and n==1:
                 return 0
-            if i==1 and j == 1:
-                return 1
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            memo[(i,j)] = dp(i-1, j) + dp(i, j-1)
-            return memo[(i,j)]
-            
-        return dp(m,n)
+            elif m ==1:
+                return 0
+            elif n==1:
+                return 0
+            if (m,n) not in memo:
+                right = dp(m,n-1)
+                down = dp(m-1, n)
+                memo[(m,n)] = 1 + right + down
+            return memo[(m,n)]
+        
+        return 1+ dp(m,n)
