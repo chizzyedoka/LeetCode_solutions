1095dd27c519e00871e87c35d0f230f827fc522e
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-03 01:21:32 +0100

    Time: 3 ms (16.13%) | Memory: 18.2 MB (5.22%) - LeetSync

--- 677-map-sum-pairs/map-sum-pairs.py
+++ 677-map-sum-pairs/map-sum-pairs.py
@@ -1,3 +1,4 @@
+# brute force
 class MapSum:
 
     def __init__(self):
@@ -14,6 +15,40 @@ class MapSum:
                 _sum += self.map[key]
         return _sum        
 
+class TrieNode:
+    def __init__(self,char):
+        self.char = char
+        self.children = dict()
+        self.score = 0 # store sum of values of all words passing this node
+
+class MapSum:
+
+    def __init__(self):
+        self.map = {} # keep track of previous key->value to handle updates
+        self.root = TrieNode("#")
+
+    def insert(self, key: str, val: int) -> None:
+        # delta = new value - old value (to handle updates properly)
+        delta = val - self.map.get(key, 0)
+        self.map[key] = val
+
+        root = self.root
+        for char in key:
+            if char not in root.children:
+                root.children[char] = TrieNode(char)
+            root = root.children[char]
+            root.score += delta   # update prefix sum along the path
+
+       
+
+    def sum(self, prefix: str) -> int:
+        root = self.root
+        for char in prefix:
+            if char not in root.children:
+                return 0
+            root = root.children[char]
+        return root.score
+               
 
 # Your MapSum object will be instantiated and called as such:
 # obj = MapSum()
