c4bb6dd832b824ea48ac2c612c3650b13920bf6b
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-16 01:36:45 +0100

    Time: 18 ms (41.41%) | Memory: 18.6 MB (73.35%) - LeetSync

--- 1775-design-an-ordered-stream/design-an-ordered-stream.py
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -3,7 +3,7 @@ class OrderedStream:
     def __init__(self, n: int):
         self.n = n
         self.pointer = 0
-        self.data = [None] * (n+1)
+        self.data = [None] * (n)
 
     def insert(self, idKey: int, value: str) -> List[str]:
         self.data[idKey-1] = value
