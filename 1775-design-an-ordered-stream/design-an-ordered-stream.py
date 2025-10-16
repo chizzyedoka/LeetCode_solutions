2c6404370c4aa261658895b73c5333f5a1584d2c
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-16 01:56:07 +0100

    Time: 10 ms (80.07%) | Memory: 18.5 MB (94.49%) - LeetSync

--- 1775-design-an-ordered-stream/design-an-ordered-stream.py
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -3,7 +3,7 @@ class OrderedStream:
     def __init__(self, n: int):
         self.n = n
         self.pointer = 0
-        self.data = [None] * (n)
+        self.data = [None] * n
 
     def insert(self, idKey: int, value: str) -> List[str]:
         self.data[idKey-1] = value
@@ -11,6 +11,22 @@ class OrderedStream:
         while self.pointer < self.n and self.data[self.pointer]:
             result.append(self.data[self.pointer])
             self.pointer += 1
+        return result
+
+class OrderedStream:
+    def __init__(self, n:int):
+        self.data = {}       # store inserted values dynamically
+        self.pointer = 1     # next id to output
+
+    def insert(self, idKey: int, value: str) -> list[str]:
+        self.data[idKey] = value
+        result = []
+
+        # Continue while the next expected id exists in data
+        while self.pointer in self.data:
+            result.append(self.data[self.pointer])
+            self.pointer += 1
+
         return result
 
 
