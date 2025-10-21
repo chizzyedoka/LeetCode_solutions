19603eefba5309b26dc0e25b93b13ed289b3da43
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-21 23:44:53 +0100

    Time: 3 ms (68.65%) | Memory: 18.1 MB (25.05%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -24,7 +24,7 @@ class Solution:
         def dfs(start):
             if start == len(s):
                 return True
-                
+
             if start in memo:
                 return memo[start]
 
@@ -37,3 +37,19 @@ class Solution:
             return False
 
         return dfs(0)
+
+# bottom up
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n+1):
+            for j in range(i):
+                prefix = s[j:i]
+                if dp[j] and prefix in wordDict:
+                    dp[i] = True
+                    break
+        return dp[n]
