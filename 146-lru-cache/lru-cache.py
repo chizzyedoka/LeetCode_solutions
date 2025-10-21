a358ad2bc87d00aa5e14b6385f79a609aa7cc5a2
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-21 18:41:51 +0100

    Time: 135 ms (60.13%) | Memory: 77.8 MB (97.34%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -9,52 +9,64 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.node_map = {}
-
+        self.cache = {} # key to Node
         self.head = ListNode(-1, -1)
-        self.tail = ListNode(-1,-1)
-
+        self.tail = ListNode(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
 
+    def _add(self, node):   # head <-> 1 < -> 2 <-> 3 <-> 4 <-> tail
+        # add node 5
+        last_node = self.tail.prev # 4 <-> tail
+        self.tail.prev = node # 5 <- tail  # 4 -> tail
+        node.next = self.tail # 5 <-> tail
+        last_node.next = node # 4 -> 5 <-> tail
+        node.prev = last_node # 4 <-> 5 <-> tail
+
+
+
+    def _remove(self, node): # 1 < -> 2 <-> 3 <-> 4
+        nxt_node = node.next # 3 <-> 4
+        prev_node = node.prev # 1
+        node.prev.next = nxt_node # 1 -> 3 <-> 4
+        nxt_node.prev = prev_node # 1 <-> 3 <-> 4
+
     def get(self, key: int) -> int:
-        if key not in self.node_map:
+        if key in self.cache:
+            node = self.cache[key]
+            # remove it from its position 
+            self._remove(node)
+            # add to the back, recently being used
+            self._add(node)
+            return node.val
+        else:
             return -1
 
-        node = self.node_map[key]
-        self._remove(node)
-        self._add(node)
-        return node.val
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.node_map:
-            old_node = self.node_map[key]
-            self._remove(old_node)
-
-        new_node = ListNode(key, value)
-        self.node_map[key] = new_node
-        self._add(new_node)
-
-        if len(self.node_map) > self.capacity:
-            node_to_delete = self.head.next
-            self._remove(node_to_delete)
-
-            del self.node_map[node_to_delete.key]
-
-    def _add(self, node):
-        prev_end = self.tail.prev
-        prev_end.next = node
-
-        node.prev = prev_end
-        node.next = self.tail
-
-        self.tail.prev = node
         
-    def _remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        # check if we are updating a key
+        if key in self.cache:
+            old_node = self.cache[key]
+            self._remove(old_node)
+            old_node.val = value # edit its value, so we dont have to delete it from dictionary
+            self._add(old_node)
+            return
 
+        # evict lru node if we've reached capacity
+        if len(self.cache) == self.capacity:
+            lru_node = self.head.next
+            self._remove(lru_node)
+            del self.cache[lru_node.key]
+            self.head.next = lru_node.next
+        
+        # add new node
+        node = ListNode(key, value)
+        self._add(node)
+        self.cache[key] = node
+        return
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
