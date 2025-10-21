4bff6db4f0b75107cbcc374196210a7e28cd9f4d
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-21 10:24:46 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (47.25%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,33 +1,32 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def dfs(start, destination, weight):
+            if start == destination:
+                return weight
+            seen.add(start)
+            for neighbor, val in graph[start]:
+                if neighbor not in seen:
+                    result = dfs(neighbor, destination, weight*val)
+                    if result != -1:
+                        return result
+            return -1
+
         result = []
         graph = defaultdict(list)
-        
-        for i, equation in enumerate(equations):
-            u,v = equation
-            graph[u].append((v,values[i]))
-            graph[v].append((u,1/values[i]))
 
-        def dfs(src,dest, weight):
-            if src == dest:
-                return weight
-
-            seen.add(src)
-            for neighbor, w in graph[src]:
-                if neighbor not in seen:
-                    res = dfs(neighbor, dest, w* weight) 
-                    if res != -1:
-                        return res
-            return -1
-           
-                
+        for i in range(len(equations)):
+            u,v = equations[i]
+            graph[u].append((v, values[i]))
+            graph[v].append((u, 1/values[i]))
 
         for query in queries:
-            u,v = query
-            if u not in graph or v not in graph:
-                result.append(-1)
-            else:
+            start, destination = query
+            if start in graph and destination in graph:
                 seen = set()
-                result.append(dfs(u,v, 1))
-
+                answer = dfs(start, destination, 1)
+                result.append(answer)
+            else:
+                result.append(-1)
         return result
+
+        
