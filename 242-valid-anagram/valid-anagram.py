b0cfbd6a8051abd991f905d393f1e92cd5b2bec4
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-28 12:41:24 +0100

    Time: 15 ms (41.24%) | Memory: 17.8 MB (64.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s)
-        t = list(t)
-        s.sort()
-        t.sort()
-        return s == t
+        if len(s) != len(t):
+            return False
+
+        s_bucket = [0] * 26
+        t_bucket = [0] * 26
+
+        for i in range(len(s)):
+            s_char, t_char = s[i], t[i]
+            s_index = ord(s_char) - ord('a')
+            t_index = ord(t_char) - ord('a')
+            s_bucket[s_index] += 1
+            t_bucket[t_index] += 1
+
+        for i in range(26):
+            if s_bucket[i] != t_bucket[i]:
+                return False
+
+        return True
