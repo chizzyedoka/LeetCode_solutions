f7885973e6f5f06b14fd6c9e92df92ca002d8f2a
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-10-30 20:34:39 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.94%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -25,30 +25,30 @@ class Solution:
 
         return result
 
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        wordDict = set(wordDict)
-        n = len(s)
-        result = []
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+#         wordDict = set(wordDict)
+#         n = len(s)
+#         result = []
 
-        def dp(start, path, result): # dp(i) asks if we can split up to index i
-            # base case
-            if start == n:
-                sentence = " ".join(path)
-                result.append(sentence)
-                return
+#         def dp(start, path, result): # dp(i) asks if we can split up to index i
+#             # base case
+#             if start == n:
+#                 sentence = " ".join(path)
+#                 result.append(sentence)
+#                 return
 
-            for end in range(start+1, n+1):
-                prefix = s[start:end]
-                if prefix in wordDict:
-                    path.append(prefix)
-                    dp(end, path, result)
+#             for end in range(start+1, n+1):
+#                 prefix = s[start:end]
+#                 if prefix in wordDict:
+#                     path.append(prefix)
+#                     dp(end, path, result)
 
-                    path.pop()
+#                     path.pop()
 
                     
                 
 
-        dp(0, [], result)
+#         dp(0, [], result)
 
-        return result
+#         return result
