7327a05cea737d799636de796888ed29150d8cf3
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-11-02 00:54:45 +0100

    Time: 151 ms (83.06%) | Memory: 31.3 MB (64.47%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,23 +1,25 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        result = []
-        q = deque()
+        # use a deque to keep the window valid -> max of k
+        # minDeque -> front of the queue is the smallest ->monotonic increasing
+        # maxDeque -> front of the deque is the maximum -> monotonic decreasing -> use this [7, 5, 3 ] 6
+
+        queue = deque()
         n = len(nums)
+        result = []
 
         for i in range(k):
-            num = nums[i]
-            while q and nums[q[-1]] <= num:
-                q.pop()
-            q.append(i)
-        result.append(nums[q[0]])
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            queue.append(i)
+        result.append(nums[queue[0]])
 
         for i in range(k, n):
-            if q and i - q[0] == k:
-                q.popleft()
-
-            while q and nums[q[-1]] <= nums[i]:
-                q.pop()
-            q.append(i)
-            result.append(nums[q[0]])
+            if queue and i - queue[0] >= k:
+                queue.popleft()
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            queue.append(i)
+            result.append(nums[queue[0]])
+        
         return result
-            
