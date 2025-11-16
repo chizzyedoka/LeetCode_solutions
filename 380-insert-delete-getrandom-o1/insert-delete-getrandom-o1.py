e90dbb1e47c4f4e1dc435ed0ca8e35f803e6b6c0
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-11-16 11:12:01 +0100

    Time: 98 ms (92.63%) | Memory: 58.1 MB (20.07%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,31 +1,35 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.itemsList = []
-        self.itemsDict = {}
-
-    def insert(self, val: int) -> bool:
-        if val not in self.itemsDict:
-            self.itemsList.append(val)
-            self.itemsDict[val] = len(self.itemsList) - 1
-            return True
-        return False
+        self.items = [] # [100, 300, 500]
+        self.dict = {} # {100: 0, 300:1, 500:2}
         
 
-    def remove(self, val: int) -> bool:
-        if val not in self.itemsDict:
+    def insert(self, val: int) -> bool:
+        if val in self.dict:
             return False
-        lastElement = self.itemsList[-1]
-        indexToRemove = self.itemsDict[val]
-        # swap lastElement and value we want to remove
-        self.itemsList[indexToRemove], self.itemsList[-1] = lastElement, self.itemsList[indexToRemove]
-        self.itemsDict[lastElement] = indexToRemove
-        self.itemsList.pop()
-        del self.itemsDict[val]
+        
+        self.dict[val] = len(self.items)
+        self.items.append(val)
+        return True
+        
+
+    def remove(self, val: int) -> bool: # remove 300
+        if val not in self.dict:
+            return False
+        indexToRemove = self.dict[val]
+        lastElement = self.items[-1]
+        # swap val with last val
+        self.items[indexToRemove], self.items[-1] = self.items[-1], self.items[indexToRemove] # [100, 500, 300]
+        self.dict[lastElement] = indexToRemove     # {100: 0, 500:1, 300:2}
+        self.items.pop()
+        del self.dict[val] # {100: 0, 500:1, 
         return True
 
+        
+
     def getRandom(self) -> int:
-        return random.choice(self.itemsList)
+        return random.choice(self.items)
         
 
 
