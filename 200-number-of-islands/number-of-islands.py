6e0eb3c8899fac773d2af2871f3b838570a5a0f6
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-12-28 17:10:34 +0100

    Time: 268 ms (28.52%) | Memory: 28.5 MB (8.29%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
         rows, cols = len(grid), len(grid[0])
         LAND = "1"
-        islands = [0]
+        islands = 0
         visited = set()
-        directions = [(-1,0), (1,0), (0,1), (0,-1)]
+        
 
-        def bfs(r,c):
-            queue = deque()
-
-            queue.append((r,c))
-            visited.add((r,c))
-            
-            while queue:
-                i, j = queue.popleft()
-                for di,dj in directions:
-                    new_i, new_j = i+di, j+dj
-                    row_inbounds = 0 <= new_i < rows
-                    col_inbounds = 0 <= new_j < cols
-                    if row_inbounds and col_inbounds and grid[new_i][new_j]== LAND and (new_i, new_j) not in visited:
-                        queue.append((new_i, new_j))
-                        visited.add((new_i, new_j))
-            islands[0] += 1
+        def dfs(r,c):
+            row_inbounds = 0 <= r < rows
+            col_inbounds = 0 <= c < cols
+            if row_inbounds and col_inbounds and grid[r][c] == LAND and (r,c) not in visited:
+                visited.add((r,c))
+                dfs(r+1, c)
+                dfs(r-1, c)
+                dfs(r, c+1)
+                dfs(r, c-1)
 
 
 
@@ -30,6 +26,7 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == LAND and (r,c) not in visited:
-                    bfs(r,c)
+                    dfs(r,c)
+                    islands += 1
 
-        return islands[0]
+        return islands
