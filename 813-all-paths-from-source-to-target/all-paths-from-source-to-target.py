3532f69346d706d2f7b55a5183858c02b13bf37d
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-12-28 22:39:12 +0100

    Time: 8 ms (14.59%) | Memory: 18.8 MB (93.05%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,73 +1,17 @@
-# iterative
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n = len(graph)
-        source, target = 0, n-1
-        stack = [( source, [source] )]
-        allPaths = []
-        while stack:
-            node, path = stack.pop()
-            if node == target:
-                allPaths.append(path)
-            for nei in graph[node]:
-                stack.append((nei, path + [nei]))
-        return allPaths
+        source, target = 0, len(graph)-1
+        paths = []
 
-# dfs
-class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n = len(graph)
-        source, target = 0, n-1
-        allPaths = []
-        
-        def dfs(node, path): 
+        def dfs(node, path):
             if node == target:
-                allPaths.append(path)
+                paths.append(list(path))
                 return
-
-            for nei in graph[node]:
-                if dfs(nei, path + [nei]):
-                    return
-            return
-
-        dfs(source, [source])
-        return allPaths
-
-# queue
-class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n = len(graph)
-        source, target = 0, n-1
-        queue = deque([( source, [source] )])
-        allPaths = []
-        while queue:
-            node, path = queue.popleft()
-            if node == target:
-                allPaths.append(path)
-            for nei in graph[node]:
-                queue.append((nei, path + [nei]))
-        return allPaths
-
-#
-class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:  
-        n = len(graph)
-        source, target = 0, n-1
-        allPaths = []
-        
-        def backtrack(node, path):
-            if node == target:
-                allPaths.append(path[:])
-                return
-
-            for neighbor in graph[node]:
-                path.append(neighbor)
-                backtrack(neighbor, path)
+            
+            for next_node in graph[node]:
+                path.append(next_node)
+                dfs(next_node, path)
                 path.pop()
 
-            return
-
-        backtrack(source, [source])
-
-        return allPaths
-
+        dfs(source, [0])
+        return paths
