114eb81976a53c8297ed9d0b0642bbc8a9af5e7d
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2025-12-29 23:10:42 +0100

    Time: 237 ms (54.85%) | Memory: 73 MB (56.44%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+
+        seen = set()
+        graph = defaultdict(list) # {1:[(2,9)],    2:[(1,9)]}
+        self.result = float("inf")
+
+        for (u, v, w) in roads:
+            graph[u].append((v,w))
+            graph[v].append((u,w))
+
+        def dfs(node):
+            seen.add(node)
+
+            for next_node, weight in graph[node]:
+                self.result = min(self.result, weight)
+                if next_node not in seen:
+                    dfs(next_node)
+
+        
+
+        dfs(1)
+        return self.result
+
+        
+
+
+        
