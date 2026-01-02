1b836a08bce57f55d883df84b055a7f39e7b6578
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-01-02 09:38:21 +0100

    Time: 39 ms (55.67%) | Memory: 19.9 MB (85.94%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,26 +1,31 @@
-class BrowserHistory:
-    
-    def __init__(self, homepage: str):
-        self.history = [homepage]
-        self.curPosition = 0
-        self.historySize = 1
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
 
-    def visit(self, url: str) -> None: #[a, b, c, d, a]
-        if len(self.history) < self.curPosition + 2:
-            self.history.append(url)
-        else:
-            self.history[self.curPosition+1] = url
-        self.curPosition += 1
-        self.historySize = self.curPosition + 1
-        
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.cur = ListNode(homepage)
+
+    def visit(self, url: str) -> None:
+        new_page = ListNode(url) # b <-> None
+        new_page.prev = self.cur # a <- b <-> None
+        self.cur.next = new_page # a <-> b <-> None
+        self.cur = self.cur.next
 
     def back(self, steps: int) -> str:
-        self.curPosition = max(self.curPosition-steps, 0)
-        return self.history[self.curPosition]
+        while self.cur.prev and steps > 0:
+            self.cur = self.cur.prev
+            steps -= 1
+        return self.cur.val
 
     def forward(self, steps: int) -> str:
-        self.curPosition = min(self.curPosition + steps, self.historySize-1)
-        return self.history[self.curPosition]
+        while self.cur.next and steps > 0:
+            self.cur = self.cur.next
+            steps -= 1
+        return self.cur.val
 
 
 # Your BrowserHistory object will be instantiated and called as such:
