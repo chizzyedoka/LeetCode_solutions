0c900d8393486e6fa3fb887880c2e33ba452d629
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-03-31 01:16:58 +0100

    Time: 71 ms (94.52%) | Memory: 19.6 MB (69.59%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,13 +7,15 @@ class Solution:
         # {A:2, B:1}
         result = 0
         start = 0
+        max_count = 0
         char_count = defaultdict(int)
         for end in range(len(s)):
             char = s[end]
             char_count[char] += 1
             window = end - start + 1
+            max_count = max(max_count, char_count[char])
             # shrink window while window is invalid
-            while window - max(char_count.values()) > k: 
+            while window - max_count > k: 
                 left_char = s[start]
                 char_count[left_char] -= 1
                 start += 1
