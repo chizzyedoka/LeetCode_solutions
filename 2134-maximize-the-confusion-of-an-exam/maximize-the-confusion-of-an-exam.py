283a0731153682d2bd996bae083c46899842a2ac
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-04-07 03:55:46 +0100

    Time: 243 ms (11.42%) | Memory: 19.5 MB (85.15%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -2,22 +2,16 @@ class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         # max count(T, F) in that window
         # slide window when k has been exhausted
-        bool_map = {0:0, 1:0}
+        bool_map = {'F':0, 'T':0}
         start = 0
         result = 0
 
         for end in range(len(answerKey)):
-            if answerKey[end] == "T":
-                bool_map[1] += 1
-            else:
-                bool_map[0] += 1
+            bool_map[answerKey[end]] += 1
 
             window = end - start + 1
             while (window - max(bool_map.values()) > k ):
-                if answerKey[start] == "T":
-                    bool_map[1] -= 1
-                else:
-                    bool_map[0] -= 1
+                bool_map[answerKey[start]] -= 1
                 start += 1
                 window = end - start + 1
 
