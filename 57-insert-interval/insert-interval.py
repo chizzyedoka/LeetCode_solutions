abd801c7f30e4432e56eac58289298aa75a56728
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-06-08 09:38:32 +0100

    Time: 0 ms (100.00%) | Memory: 21.3 MB (88.71%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,16 +1,33 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals.sort()
+        def binary_search(arr, num):
+            left, right = 0, len(arr) 
+            while left < right:
+                mid = (left + right) // 2
+                if arr[mid] <= num:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
 
-        merged_intervals = [intervals[0]]
+        if not intervals:
+            return [newInterval]
 
-        for (start, end) in intervals[1:]:
-            prev_start, prev_end = merged_intervals[-1]
-
-            if prev_end >= start:
-                merged_intervals[-1] = [prev_start, max(prev_end, end)]
-            else:
-                merged_intervals.append([start, end])
+        start_times = [interval[0] for interval in intervals]
+        insert_index = binary_search(start_times, newInterval[0])
+       
+        intervals.insert(insert_index, newInterval)
+        merged_intervals = []
         
+        for (start, end) in intervals:
+            if not merged_intervals:
+                merged_intervals.append([start, end])
+            else:
+                prev_start, prev_end = merged_intervals[-1]
+
+                if prev_end >= start:
+                    merged_intervals[-1][1] = max(prev_end, end)
+                else:
+                    merged_intervals.append([start, end])
         return merged_intervals
+        
