42f876176c4ffcde38a59f779feeb7085bd2f529
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-06-14 16:03:18 +0100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -10,3 +10,18 @@ class Solution:
         for i in range(3, n+1):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {0:0, 1:1, 2:2}
+        
+        def dp(i):
+            if i <= 2:
+                return i
+            if i not in memo:
+                memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
+
+        return dp(n)
+        
+        
