f50c46009d28bd7b92f212f8aa4adf938f731eef
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-06-14 16:41:32 +0100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (61.80%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        # if there's just one house, pick that
+        # if there's just two houses, pick the max btw first and second house
+        # if there's three houses we can either choose to rob the first and third house or just the second house 
+        # dp[3] = max(dp[1]+nums[3], dp[2])
+
+        # if there's four houses
+        # dp[4] = max(nums[4] + dp[2], dp[3])
+        # where dp[i] is the max money a robber can steal at house i
+
+        # [1, 2, 3, 1]
+        n = len(nums)
+        if n == 1:
             return nums[0]
-        dp = [0] * len(nums)
+        dp = [0] * n 
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2,len(nums)):
-            dp[i] = max(dp[i-2]+ nums[i], dp[i-1])
-        return dp[-1]
+        dp[1] = max(nums[0], nums[1]) 
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        return dp[n-1]
