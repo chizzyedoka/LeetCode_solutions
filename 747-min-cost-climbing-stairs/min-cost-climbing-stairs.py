25c33575ffc7df46bf507bd7f31bbdb764b961ad
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-06-16 10:29:37 +0100

    Time: 4 ms (30.16%) | Memory: 21.9 MB (8.73%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -10,3 +10,21 @@ class Solution:
             dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
         return dp[n]
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+
+        if n == 2:
+            return min(cost[0], cost[1])
+
+        memo = {}
+
+        def dp(i):
+            if i < 2:
+                return 0
+            if i not in memo:
+                memo[i] =  min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
+            return memo[i]
+
+        return dp(n)
