660c149ecffd3fb757862e940649bd816ffd8d50
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-06-23 11:10:27 +0100

    Time: 295 ms (87.07%) | Memory: 44.4 MB (45.08%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -27,4 +27,18 @@ class Solution:
             return memo[(i,j)]
 
         return dp(0,0)
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1+dp[i+1][j+1] 
+                else:
+                    dp[i][j] = max( dp[i+1][j], dp[i][j+1] )
+        return dp[0][0]
             
