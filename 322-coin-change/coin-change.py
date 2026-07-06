afbaedc3246732fe194e2ac2f7dc757dce15a3e0
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-07-06 14:42:53 +0100

    Time: 507 ms (64.33%) | Memory: 19.8 MB (35.62%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -17,3 +17,18 @@ class Solution:
             return memo[curAmount]
             
         return dfs(amount)
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] * (amount + 1)
+        coins.sort()
+
+        for i in range(1, amount+1):
+            minCoins = float('inf')
+            for coin in coins:
+                diff = i - coin
+                if diff < 0:
+                    break
+                minCoins = min(minCoins, 1+dp[diff])
+            dp[i] = minCoins 
+        return dp[-1] if dp[-1] != float('inf') else -1
