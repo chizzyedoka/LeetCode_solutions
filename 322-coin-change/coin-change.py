a8028baf6d189647391c9d5e3cfdaa887c3fe9e0
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-07-06 14:19:07 +0100

    Time: 1313 ms (6.23%) | Memory: 68.5 MB (14.92%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,30 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
         memo = {}
-        # state dp
-        def dp(remAmount): 
-            # base case
-            if remAmount < 0:
-                return float('inf')
-            if remAmount == 0:
+        def dfs(curAmount):
+            if curAmount == 0:
                 return 0
+            if curAmount < 0:
+                return -1
 
-            # recursive solution
-            min_coins = float("inf")
-
-            if remAmount in memo:
-                    return memo[remAmount]
-
-            for coin in coins:
-                res = dp(remAmount - coin)
-                if res != float('inf'):
-                    min_coins = min(min_coins, 1 + res)
-            memo[remAmount] = min_coins
+            minCoins = float('inf')
+            if curAmount not in memo:
+                for coin in coins:
+                    res = dfs(curAmount - coin)
+                    if res != -1:
+                        minCoins = min(minCoins, 1+res)
+                memo[curAmount] = minCoins if minCoins != float('inf') else -1
+            return memo[curAmount]
             
-            return min_coins
-
-        result = dp(amount)
-        return result if result != float("inf") else -1
-
-             
+        return dfs(amount)
