79fc7f9c04ecad65063bf781653066a1163fc8c3
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-09-02 13:14:51 +0100

    Time: 0 ms (100.00%) | Memory: 20.5 MB (18.22%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # map, cause we need the index
+        # for each element, check if the element's  compliment exits in the set
+        # if it does return their indexes
+        # else store in the set the element and its index
         hashmap = {}
-
-        for i,num in enumerate(nums):
-            complement = target - num
-            if complement in hashmap:
-                return [i,hashmap[complement]]
+        for i in range(len(nums)):
+            num = nums[i]
+            compliment = target - num
+            if compliment in hashmap:
+                return [i, hashmap[compliment]]
             hashmap[num] = i
