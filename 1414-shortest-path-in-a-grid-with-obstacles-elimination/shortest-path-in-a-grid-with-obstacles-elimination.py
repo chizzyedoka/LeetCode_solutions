3a8ec2152d3fab80a53d9dae95aade1631bda6be
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-09-12 18:15:45 +0100

    Time: 337 ms (37.25%) | Memory: 28.3 MB (31.51%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,27 +1,34 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
+
+        rows, cols = len(grid), len(grid[0])
+        EMPTY = 0
         OBSTACLE = 1
-        directions = [(0, 1), (0,-1), (1,0), (-1, 0)]
-        # (row, col, remaining_k)
-        q = deque()
-        q.append((0,0,k,0))
-        visited = set((0,0,k))
 
-        while q:
-            (r, c, rem, steps) = q.popleft()
+        queue = deque([(0,0, k, 0)])
+        visited = {(0,0, k)}
+        destination = (rows-1, cols-1)
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
-            # reached destination
-            if (r,c) == (m-1, n-1):
-                return steps
-
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < m and 0 <= nc < n:
-                    if grid[nr][nc] == OBSTACLE:
-                        new_rem = rem - 1
-                    else: new_rem = rem
-                    if new_rem >= 0 and (nr,nc, new_rem) not in visited:
-                        visited.add((nr,nc, new_rem))
-                        q.append((nr,nc, new_rem, steps+1))
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r, c, k, distance = queue.popleft()
+                if (r,c) == destination:
+                    return distance
+                for (dr, dc) in directions:
+                    nr, nc = r+dr, c+dc
+                    row_inbounds = 0<= nr < rows
+                    col_inbounds = 0 <= nc < cols
+    
+                    if row_inbounds and col_inbounds:
+                        if grid[nr][nc] == OBSTACLE: 
+                            k_left = k - 1
+                        else:
+                            k_left = k
+                            
+                        if k_left >= 0 and (nr, nc, k_left) not in visited:
+                            queue.append((nr,nc, k_left, distance+1))
+                            visited.add((nr,nc, k_left))
+            
         return -1
