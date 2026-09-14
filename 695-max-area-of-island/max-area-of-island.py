1667d6d92e1167135308e881d0d488696a3621ff
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-09-14 09:20:29 +0100

    Time: 31 ms (14.92%) | Memory: 24.1 MB (8.16%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,29 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
         rows, cols = len(grid), len(grid[0])
-        area = 0
-        max_area = 0
-        LAND, WATER = 1, 0
+        LAND = 1
+        self.max_area = 0
+        self.area = 0
         visited = set()
 
         def dfs(r,c):
-            rowInbounds = 0 <= r < rows
-            colInbounds = 0 <= c < cols
-            if not rowInbounds or not colInbounds or (r,c) in visited or grid[r][c] == WATER:
-                return 0
+            row_inbounds = 0 <= r < rows
+            col_inbounds = 0 <= c < cols
 
-            visited.add((r,c))
-            area = 1 +  (dfs(r+1,c) +
-                    dfs(r-1,c) +
-                    dfs(r,c+1) +
-                    dfs(r,c-1))
-            return area
+            if row_inbounds and col_inbounds and grid[r][c] == LAND and (r,c) not in visited:
+                visited.add((r,c))
+                self.area += 1
+                dfs(r+1,c)
+                dfs(r-1, c)
+                dfs(r, c+1)
+                dfs(r, c-1)
+            else:
+                return
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == LAND:
-                    area = dfs(r,c)
-                    max_area = max(area, max_area)
-                   
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == LAND and (i,j) not in visited:
+                    self.area = 0
+                    dfs(i,j)
+                    self.max_area = max(self.max_area, self.area)
 
-        return max_area
+        return self.max_area
+
