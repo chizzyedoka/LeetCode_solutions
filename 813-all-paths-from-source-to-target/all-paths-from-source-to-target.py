b0d111a13cc4e8ec37a0e7e886a79558affc54f8
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-09-14 08:45:06 +0100

    Time: 3 ms (92.01%) | Memory: 20.5 MB (81.55%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -16,3 +16,20 @@ class Solution:
             for next_node in graph[node]:
                 stack.append((next_node, path + [next_node]))
         return result
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        source, target = 0, len(graph)-1
+        paths = []
+
+        def dfs(node, path):
+            path.append(node)
+            if node == target:
+                paths.append(list(path))
+            else:
+                for next_node in graph[node]:
+                    dfs(next_node, path)
+            path.pop()
+
+        dfs(source, [])
+        return paths
