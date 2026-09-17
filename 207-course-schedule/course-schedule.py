1563515f7f8304a801a2bfb43d92bb5622f11fc7
Edoka Chisom <59281535+chizzyedoka@users.noreply.github.com>
2026-09-17 07:24:55 +0100

    Time: 4 ms (64.08%) | Memory: 21.3 MB (25.62%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,36 +1,32 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        graph = defaultdict(list)
         
-        def graphFormation(courses):
-            graph = defaultdict(list)
-            for (u,v) in courses:
-                graph[u].append(v)
-            return graph
-
-        def dfs(node):
-            state = states[node]
-            if state == VISITING:
-                return False
-            elif state ==VISITED:
-                return True
-
-            states[node] = VISITING
-            for i in graph[node]:
-                if not dfs(i):
-                    return False
-            
-            states[node] = VISITED
-            return True
-
-
-        graph = graphFormation(prerequisites)
         UNVISITED = 0
         VISITING = 1
         VISITED = 2
-        states = [0] * numCourses
-        
 
+        states = [UNVISITED] * numCourses
+
+        for (u,v) in prerequisites:
+            graph[u].append(v)
+
+        def dfs(node):
+            if states[node] == VISITING:
+                return False
+            if states[node] == VISITED:
+                return True
+
+            states[node] = VISITING
+
+            for new_node in graph[node]:
+                if not dfs(new_node):
+                    return False
+            states[node] = VISITED
+            return True
+        
         for i in range(numCourses):
             if not dfs(i):
                 return False
+            
         return True
